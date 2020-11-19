6dc3e8efefb1c7867df9e349aa89fc3a8b356e3e
Tommy Ettinger <tommy.ettinger@gmail.com>
2020-11-18 19:35:28 -0800

    Bring over Bresenham and OrthoLine.
    These will probably be changed so they either implement an interface that allows them to be plugged into an LOS setup, or so they individually can do what LOS does.

---
+++ squidplace/src/main/java/com/github/yellowstonegames/place/Bresenham.java
@@ -0,0 +1,236 @@
+package com.github.yellowstonegames.place;
+
+import com.github.tommyettinger.ds.ObjectList;
+import java.util.List;
+
+/**
+ * Provides a means to generate Bresenham lines in 2D and 3D.
+ *
+ * @author Eben Howard - http://squidpony.com - howard@squidpony.com
+ * @author Lewis Potter
+ * @author Tommy Ettinger
+ * @author smelC
+ */
+public class Bresenham {
+
+    /**
+     * Prevents any instances from being created
+     */
+    private Bresenham() {
+    }
+
+    /**
+     * Generates a 2D Bresenham line between two points. If you don't need
+     * the {@link List} interface for the returned reference, consider
+     * using {@link #lineArray(Coord, Coord)} to save some memory.
+     *
+     * @param a the starting point
+     * @param b the ending point
+     * @return The path between {@code a} and {@code b}.
+     */
+    public static List<Coord> line(Coord a, Coord b) {
+        return line(a.x, a.y, b.x, b.y);
+    }
+
+    /**
+     * Generates a 2D Bresenham line between two points.
+     *
+     * @param a the starting point
+     * @param b the ending point
+     * @return The path between {@code a} and {@code b}.
+     */
+    public static Coord[] lineArray(Coord a, Coord b) {
+        return lineArray(a.x, a.y, b.x, b.y);
+    }
+
+    /**
+     * Generates a 2D Bresenham line between two points. If you don't need
+     * the {@link List} interface for the returned reference, consider
+     * using {@link #lineArray(int, int, int, int)} to save some memory.
+     * <br>
+     * Uses ordinary Coord values for points, and these can be pooled
+     * if they are within what the current pool allows (it starts,
+     * by default, pooling Coords with x and y between -3 and 255,
+     * inclusive). If the Coords are pool-able, it can significantly
+     * reduce the work the garbage collector needs to do, especially
+     * on Android.
+     *
+     * @param startx the x coordinate of the starting point
+     * @param starty the y coordinate of the starting point
+     * @param endx the x coordinate of the starting point
+     * @param endy the y coordinate of the starting point
+     * @return a ObjectList (internally, an ObjectList) of Coord points along the line
+     */
+    public static List<Coord> line(int startx, int starty, int endx, int endy) {
+        // largest positive int for maxLength; a ObjectList cannot actually be given that many elements on the JVM
+        return line(startx, starty, endx, endy, 0x7fffffff);
+    }
+
+    /**
+     * Generates a 2D Bresenham line between two points, stopping early if
+     * the number of Coords returned reaches maxLength. If you don't need
+     * the {@link ObjectList} interface for the returned reference, consider
+     * using {@link #lineArray(int, int, int, int, int)} to save some memory.
+     * <br>
+     * Uses ordinary Coord values for points, and these can be pooled
+     * if they are within what the current pool allows (it starts,
+     * by default, pooling Coords with x and y between -3 and 255,
+     * inclusive). If the Coords are pool-able, it can significantly
+     * reduce the work the garbage collector needs to do, especially
+     * on Android.
+     *
+     * @param startx the x coordinate of the starting point
+     * @param starty the y coordinate of the starting point
+     * @param endx the x coordinate of the starting point
+     * @param endy the y coordinate of the starting point
+     * @param maxLength the largest count of Coord points this can return; will stop early if reached
+     * @return an ObjectList of Coord points along the line
+     */
+    public static List<Coord> line(int startx, int starty, int endx, int endy, int maxLength) {
+        int dx = endx - startx;
+        int dy = endy - starty;
+
+        int ax = Math.abs(dx);
+        int ay = Math.abs(dy);
+
+        ObjectList<Coord> result = new ObjectList<>(Math.max(ax, ay));
+
+        ax <<= 1;
+        ay <<= 1;
+        
+        int signx = (dx >> 31 | -dx >>> 31); // project nayuki signum
+        int signy = (dy >> 31 | -dy >>> 31); // project nayuki signum
+
+        int x = startx;
+        int y = starty;
+
+        int deltax, deltay;
+        if (ax >= ay) /* x dominant */ {
+            deltay = ay - (ax >> 1);
+            while (result.size() < maxLength) {
+                result.add(Coord.get(x, y));
+                if (x == endx) {
+                    return result;
+                }
+
+                if (deltay >= 0) {
+                    y += signy;
+                    deltay -= ax;
+                }
+
+                x += signx;
+                deltay += ay;
+            }
+        } else /* y dominant */ {
+            deltax = ax - (ay >> 1);
+            while (result.size() < maxLength) {
+                result.add(Coord.get(x, y));
+                if (y == endy) {
+                    return result;
+                }
+
+                if (deltax >= 0) {
+                    x += signx;
+                    deltax -= ay;
+                }
+
+
+                y += signy;
+                deltax += ax;
+            }
+        }
+        return result;
+    }
+
+
+    /**
+     * Generates a 2D Bresenham line between two points. Returns an array
+     * of Coord instead of a ObjectList.
+     * <br>
+     * Uses ordinary Coord values for points, and these can be pooled
+     * if they are within what the current pool allows (it starts,
+     * by default, pooling Coords with x and y between -3 and 255,
+     * inclusive). If the Coords are pool-able, it can significantly
+     * reduce the work the garbage collector needs to do, especially
+     * on Android.
+     *
+     * @param startx the x coordinate of the starting point
+     * @param starty the y coordinate of the starting point
+     * @param endx the x coordinate of the starting point
+     * @param endy the y coordinate of the starting point
+     * @return an array of Coord points along the line
+     */
+    public static Coord[] lineArray(int startx, int starty, int endx, int endy) {
+        // largest positive int for maxLength; it is extremely unlikely that this could be reached
+        return lineArray(startx, starty, endx, endy, 0x7fffffff);
+    }
+
+
+    /**
+     * Generates a 2D Bresenham line between two points, stopping early if
+     * the number of Coords returned reaches maxLength.. Returns an array
+     * of Coord instead of a List.
+     * <br>
+     * Uses ordinary Coord values for points, and these can be pooled
+     * if they are within what the current pool allows (it starts,
+     * by default, pooling Coords with x and y between -3 and 255,
+     * inclusive). If the Coords are pool-able, it can significantly
+     * reduce the work the garbage collector needs to do, especially
+     * on Android.
+     *
+     * @param startx the x coordinate of the starting point
+     * @param starty the y coordinate of the starting point
+     * @param endx the x coordinate of the starting point
+     * @param endy the y coordinate of the starting point
+     * @param maxLength the largest count of Coord points this can return; will stop early if reached
+     * @return an array of Coord points along the line
+     */
+    public static Coord[] lineArray(int startx, int starty, int endx, int endy, int maxLength) {
+        int dx = endx - startx;
+        int dy = endy - starty;
+
+        int signx = (dx >> 31 | -dx >>> 31); // project nayuki signum
+        int signy = (dy >> 31 | -dy >>> 31); // project nayuki signum
+
+        int ax = (dx = Math.abs(dx)) << 1;
+        int ay = (dy = Math.abs(dy)) << 1;
+
+        int x = startx;
+        int y = starty;
+
+        int deltax, deltay;
+        if (ax >= ay) /* x dominant */ {
+            deltay = ay - (ax >> 1);
+            Coord[] result = new Coord[Math.min(maxLength, dx+1)];
+            for (int i = 0; i <= dx && i < maxLength; i++) {
+                result[i] = Coord.get(x, y);
+
+                if (deltay >= 0) {
+                    y += signy;
+                    deltay -= ax;
+                }
+
+                x += signx;
+                deltay += ay;
+            }
+            return result;
+        } else /* y dominant */ {
+            deltax = ax - (ay >> 1);
+            Coord[] result = new Coord[Math.min(maxLength, dy+1)];
+            for (int i = 0; i <= dy && i < maxLength; i++) {
+                result[i] = Coord.get(x, y);
+
+                if (deltax >= 0) {
+                    x += signx;
+                    deltax -= ay;
+                }
+
+
+                y += signy;
+                deltax += ax;
+            }
+            return result;
+        }
+    }
+
+}
---
+++ squidplace/src/main/java/com/github/yellowstonegames/place/OrthoLine.java
@@ -0,0 +1,244 @@
+package com.github.yellowstonegames.place;
+
+import com.github.tommyettinger.ds.ObjectList;
+import java.util.List;
+
+/**
+ * A simple line-drawing algorithm that only takes orthogonal steps; may be useful for LOS in games that use Manhattan
+ * distances for measurements.
+ * Algorithm is from http://www.redblobgames.com/grids/line-drawing.html#stepping , thanks Amit!
+ * Created by Tommy Ettinger on 1/10/2016.
+ */
+public class OrthoLine {
+    /**
+     * Draws a line from (startX, startY) to (endX, endY) using only N/S/E/W movement. Returns a List of Coord in order.
+     *
+     * @param startX x of starting point
+     * @param startY y of starting point
+     * @param endX   x of ending point
+     * @param endY   y of ending point
+     * @return List of Coord, including (startX, startY) and (endX, endY) and all points walked between
+     */
+    public static List<Coord> line(int startX, int startY, int endX, int endY) {
+        int dx = endX - startX, dy = endY - startY, nx = Math.abs(dx), ny = Math.abs(dy);
+        int signX = (dx > 0) ? 1 : -1, signY = (dy > 0) ? 1 : -1, workX = startX, workY = startY;
+        ObjectList<Coord> drawn = new ObjectList<>(1 + nx + ny);
+        drawn.add(Coord.get(startX, startY));
+        for (int ix = 0, iy = 0; ix < nx || iy < ny; ) {
+            if ((0.5f + ix) / nx < (0.5 + iy) / ny) {
+                workX += signX;
+                ix++;
+            } else {
+                workY += signY;
+                iy++;
+            }
+            drawn.add(Coord.get(workX, workY));
+        }
+        return drawn;
+    }
+
+    /**
+     * Draws a line from start to end using only N/S/E/W movement. Returns a List of Coord in order.
+     * @param start starting point
+     * @param end ending point
+     * @return List of Coord, including start and end and all points walked between
+     */
+    public static List<Coord> line(Coord start, Coord end)
+    {
+        return line(start.x, start.y, end.x, end.y);
+    }
+    /**
+     * Draws a line from (startX, startY) to (endX, endY) using only N/S/E/W movement. Returns an array of Coord in order.
+     *
+     * @param startX x of starting point
+     * @param startY y of starting point
+     * @param endX   x of ending point
+     * @param endY   y of ending point
+     * @return array of Coord, including (startX, startY) and (endX, endY) and all points walked between
+     */
+    public static Coord[] lineArray(int startX, int startY, int endX, int endY) {
+        int dx = endX - startX, dy = endY - startY, nx = Math.abs(dx), ny = Math.abs(dy);
+        int signX = (dx > 0) ? 1 : -1, signY = (dy > 0) ? 1 : -1, workX = startX, workY = startY;
+        Coord[] drawn = new Coord[nx + ny + 1];
+        drawn[0] = Coord.get(startX, startY);
+        for (int i = 1, ix = 0, iy = 0; ix < nx || iy < ny; i++) {
+            if ((0.5f + ix) / nx < (0.5 + iy) / ny) {
+                workX += signX;
+                ix++;
+            } else {
+                workY += signY;
+                iy++;
+            }
+            drawn[i] = Coord.get(workX, workY);
+        }
+        return drawn;
+    }
+
+    /**
+     * Draws a line from start to end using only N/S/E/W movement. Returns a List of Coord in order.
+     * @param start starting point
+     * @param end ending point
+     * @return List of Coord, including start and end and all points walked between
+     */
+    public static Coord[] lineArray(Coord start, Coord end)
+    {
+        return lineArray(start.x, start.y, end.x, end.y);
+    }
+
+    /**
+     * Given an array of Coord as produced by {@link #lineArray(Coord, Coord)} or {@link #lineArray(int, int, int, int)}, this
+     * gets a char array of box-drawing characters that connect when drawn at the corresponding Coord positions in the
+     * given line. This can be useful for drawing highlight lines or showing what path something will take, as long as
+     * it only uses 4-way orthogonal connections between Coords. Any connections that require a diagonal will not be
+     * handled by this method (returning a straight line without much accuracy), and any Coords that aren't adjacent
+     * will cause an {@link IllegalStateException} if this has to draw a line between them. If this method is called on
+     * the result of this class' lineArray() method, then it should always return a valid result; if it is called on a path
+     * made with some other method, then it shouldn't throw an
+     * exception but may produce a low-quality (visually disconnected) line.
+     * @param line a Coord array where each Coord is orthogonally adjacent to its neighbor(s) in the array; usually
+     *             produced via {@link #lineArray(Coord, Coord)} or {@link #lineArray(int, int, int, int)}
+     * @return a char array of box-drawing chars that will connect when drawn at the same points as in line
+     */
+    public static char[] lineChars(Coord[] line)
+    {
+        // ─│┌┐└┘├┤┬┴┼
+        if(line == null) return null;
+        int len = line.length;
+        if(len == 0) return new char[0];
+        if(len == 1) return new char[]{'─'};
+        char[] cs = new char[len];
+        cs[0] = line[0].x == line[1].x ? '│' : '─';
+        cs[len - 1] = line[len - 1].x == line[len - 2].x ? '│' : '─';
+        Coord before, current, next;
+        for (int i = 1; i < len - 1; i++) {
+            before = line[i-1];
+            current = line[i];
+            next = line[i+1];
+            switch (before.toGoTo(current))
+            {
+                case RIGHT:
+                    switch (current.toGoTo(next))
+                    {
+                        case UP: cs[i] = '┘';
+                            break;
+                        case DOWN: cs[i] = '┐';
+                            break;
+                        default: cs[i] = '─';
+                            break;
+                    }
+                    break;
+                case LEFT:
+                    switch (current.toGoTo(next))
+                    {
+                        case UP: cs[i] = '└';
+                            break;
+                        case DOWN: cs[i] = '┌';
+                            break;
+                        default: cs[i] = '─';
+                            break;
+                    }
+                    break;
+                case UP:
+                    switch (current.toGoTo(next))
+                    {
+                        case LEFT: cs[i] = '┐';
+                            break;
+                        case RIGHT: cs[i] = '┌';
+                            break;
+                        default: cs[i] = '│';
+                            break;
+                    }
+                    break;
+                default:
+                    switch (current.toGoTo(next))
+                    {
+                        case LEFT: cs[i] = '┘';
+                            break;
+                        case RIGHT: cs[i] = '└';
+                            break;
+                        default: cs[i] = '│';
+                            break;
+                    }
+            }
+        }
+        return cs;
+    }
+    /**
+     * Given a List of Coord as produced by {@link #line(Coord, Coord)} or {@link #line(int, int, int, int)}, this
+     * gets a char array of box-drawing characters that connect when drawn at the corresponding Coord positions in the
+     * given line. This can be useful for drawing highlight lines or showing what path something will take, as long as
+     * it only uses 4-way orthogonal connections between Coords. Any connections that require a diagonal will not be
+     * handled by this method (returning a straight line without much accuracy), and any Coords that aren't adjacent
+     * will cause an {@link IllegalStateException} if this has to draw a line between them. If this method is called on
+     * the result of this class' line() method, then it should always return a valid result; if it is called on a path
+     * made with some other method, then it shouldn't throw an
+     * exception but may produce a low-quality (visually disconnected) line.
+     * @param line a List of Coord where each Coord is orthogonally adjacent to its neighbor(s) in the List; usually
+     *             produced via {@link #line(Coord, Coord)} or {@link #line(int, int, int, int)}
+     * @return a char array of box-drawing chars that will connect when drawn at the same points as in line
+     */
+    public static char[] lineChars(List<Coord> line)
+    {
+        // ─│┌┐└┘├┤┬┴┼
+        if(line == null) return null;
+        int len = line.size();
+        if(len == 0) return new char[0];
+        if(len == 1) return new char[]{'─'};
+        char[] cs = new char[len];
+        cs[0] = line.get(0).x == line.get(1).x ? '│' : '─';
+        cs[len - 1] = line.get(len - 1).x == line.get(len - 2).x ? '│' : '─';
+        Coord before, current, next;
+        for (int i = 1; i < len - 1; i++) {
+            before = line.get(i-1);
+            current = line.get(i);
+            next = line.get(i+1);
+            switch (before.toGoTo(current))
+            {
+                case RIGHT:
+                    switch (current.toGoTo(next))
+                    {
+                        case UP: cs[i] = '┘';
+                            break;
+                        case DOWN: cs[i] = '┐';
+                            break;
+                        default: cs[i] = '─';
+                            break;
+                    }
+                    break;
+                case LEFT:
+                    switch (current.toGoTo(next))
+                    {
+                        case UP: cs[i] = '└';
+                            break;
+                        case DOWN: cs[i] = '┌';
+                            break;
+                        default: cs[i] = '─';
+                            break;
+                    }
+                    break;
+                case UP:
+                    switch (current.toGoTo(next))
+                    {
+                        case LEFT: cs[i] = '┐';
+                            break;
+                        case RIGHT: cs[i] = '┌';
+                            break;
+                        default: cs[i] = '│';
+                            break;
+                    }
+                    break;
+                default:
+                    switch (current.toGoTo(next))
+                    {
+                        case LEFT: cs[i] = '┘';
+                            break;
+                        case RIGHT: cs[i] = '└';
+                            break;
+                        default: cs[i] = '│';
+                            break;
+                    }
+            }
+        }
+        return cs;
+    }
+}
