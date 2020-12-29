2cab827f59abd7c098ea545ede9ab2901c75f313
Tommy Ettinger <tommy.ettinger@gmail.com>
2020-12-29 15:35:03 -0800

    MSDF works!

--- squidglyph/src/main/java/com/github/yellowstonegames/glyph/Font.java
+++ squidglyph/src/main/java/com/github/yellowstonegames/glyph/Font.java
@@ -3,18 +3,56 @@ package com.github.yellowstonegames.glyph;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.github.tommyettinger.ds.IntObjectMap;
 import com.github.yellowstonegames.core.DigitTools;
 
 public class Font {
     public IntObjectMap<TextureRegion> mapping;
     public Texture parentTexture;
+    public boolean isMSDF;
+    public float msdfCrispness = 1.2f;
+    public float cellWidth = 1f, cellHeight = 1f;
+
+    public static final String vertexShader = "attribute vec4 " + ShaderProgram.POSITION_ATTRIBUTE + ";\n"
+            + "attribute vec4 " + ShaderProgram.COLOR_ATTRIBUTE + ";\n"
+            + "attribute vec2 " + ShaderProgram.TEXCOORD_ATTRIBUTE + "0;\n"
+            + "uniform mat4 u_projTrans;\n"
+            + "varying vec4 v_color;\n"
+            + "varying vec2 v_texCoords;\n"
+            + "\n"
+            + "void main() {\n"
+            + "	v_color = " + ShaderProgram.COLOR_ATTRIBUTE + ";\n"
+            + "	v_color.a = v_color.a * (255.0/254.0);\n"
+            + "	v_texCoords = " + ShaderProgram.TEXCOORD_ATTRIBUTE + "0;\n"
+            + "	gl_Position =  u_projTrans * " + ShaderProgram.POSITION_ATTRIBUTE + ";\n"
+            + "}\n";
+    public static final String msdfFragmentShader =  "#ifdef GL_ES\n"
+            + "	precision mediump float;\n"
+            + "	precision mediump int;\n"
+            + "#endif\n"
+            + "\n"
+            + "uniform sampler2D u_texture;\n"
+            + "uniform float u_smoothing;\n"
+            + "varying vec4 v_color;\n"
+            + "varying vec2 v_texCoords;\n"
+            + "\n"
+            + "void main() {\n"
+            + "  vec3 sdf = texture2D(u_texture, v_texCoords).rgb;\n"
+            + "  gl_FragColor = vec4(v_color.rgb, clamp((max(min(sdf.r, sdf.g), min(max(sdf.r, sdf.g), sdf.b)) - 0.5) * u_smoothing + 0.5, 0.0, 1.0) * v_color.a);\n"
+            + "}\n";
+    public static ShaderProgram msdfShader = null;
 
     private static int indexAfter(String text, String search, int from){
-        return text.indexOf(search, from < 0 ? text.length() : from) + search.length();
+        return ((from = text.indexOf(search, from)) < 0 ? text.length() : from + search.length());
     }
     public Font(String fntName, String textureName, boolean isMSDF) {
+        this.isMSDF = isMSDF;
+        if(isMSDF && msdfShader == null) {
+            msdfShader = new ShaderProgram(vertexShader, msdfFragmentShader);
+        }
         FileHandle fntHandle, textureHandle;
         String fnt;
         if((textureHandle = Gdx.files.internal(textureName)).exists()
@@ -38,13 +76,31 @@ public class Font {
         int idx = indexAfter(fnt, "\nchars count=", 0);
         int size = DigitTools.intFromDec(fnt, idx, idx = indexAfter(fnt, "\nchar id=", idx));
         mapping = new IntObjectMap<>(size);
-        for (int i = 0; i < size ; i++) {
+        for (int i = 0; i < size; i++) {
             int c = DigitTools.intFromDec(fnt, idx, idx = indexAfter(fnt, " x=", idx));
             int x = DigitTools.intFromDec(fnt, idx, idx = indexAfter(fnt, " y=", idx));
             int y = DigitTools.intFromDec(fnt, idx, idx = indexAfter(fnt, " width=", idx));
             int w = DigitTools.intFromDec(fnt, idx, idx = indexAfter(fnt, " height=", idx));
             int h = DigitTools.intFromDec(fnt, idx, idx = indexAfter(fnt, "\nchar id=", idx));
+            cellWidth = w;
+            cellHeight = h;
             mapping.put(c, new TextureRegion(parentTexture, x, y, w, h));
+        }
+    }
+    public Font scale(float horizontal, float vertical) {
+        cellWidth *= horizontal;
+        cellHeight *= vertical;
+        msdfCrispness = (msdfCrispness - 1f) / Math.max(horizontal, vertical) + 1f;
+        return this;
+    }
+    public void enableShader(Batch batch) {
+        if(isMSDF){
+            if(!batch.getShader().equals(msdfShader))
+                batch.setShader(msdfShader);
+            msdfShader.setUniformf("u_smoothing", 0.09375f * msdfCrispness * cellWidth);
+        }
+        else {
+            batch.setShader(null);
         }
     }
 }
--- squidglyph/src/test/java/com/github/yellowstonegames/glyph/FontTest.java
+++ squidglyph/src/test/java/com/github/yellowstonegames/glyph/FontTest.java
@@ -1,45 +1,63 @@
 package com.github.yellowstonegames.glyph;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.github.tommyettinger.ds.support.BitConversion;
+import com.github.tommyettinger.ds.support.LaserRandom;
+import com.github.yellowstonegames.core.DescriptiveColor;
 
 public class FontTest extends ApplicationAdapter {
 
     Font font;
     SpriteBatch batch;
+    LaserRandom random;
     public static void main(String[] args){
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
         config.setTitle("Font test");
         config.setWindowedMode(640, 480);
         config.disableAudio(true);
+        config.useVsync(false);
         new Lwjgl3Application(new FontTest(), config);
     }
 
     @Override
     public void create() {
+        random = new LaserRandom();
         batch = new SpriteBatch();
-        font = new Font("Cozette.fnt", "Cozette.png", false);
+//        font = new Font("Cozette.fnt", "Cozette.png", false);
+        font = new Font("Iosevka-Slab-Family-msdf.fnt", "Iosevka-Slab-Family-msdf.png", true).scale(2f, 2f);
     }
 
     @Override
     public void render() {
-        int x = 20, y = 200;
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        float x = 0, y = 200, w = font.cellWidth, h = font.cellHeight;
         batch.begin();
-        batch.draw(font.mapping.get('H'), x += 7, y);
-        batch.draw(font.mapping.get('e'), x += 7, y);
-        batch.draw(font.mapping.get('l'), x += 7, y);
-        batch.draw(font.mapping.get('l'), x += 7, y);
-        batch.draw(font.mapping.get('o'), x += 7, y);
-        batch.draw(font.mapping.get(','), x += 7, y);
-        x += 7;
-        batch.draw(font.mapping.get('W'), x += 7, y);
-        batch.draw(font.mapping.get('o'), x += 7, y);
-        batch.draw(font.mapping.get('r'), x += 7, y);
-        batch.draw(font.mapping.get('l'), x += 7, y);
-        batch.draw(font.mapping.get('d'), x += 7, y);
-        batch.draw(font.mapping.get('!'), x += 7, y);
+        font.enableShader(batch);
+        batch.setPackedColor(BitConversion.reversedIntBitsToFloat(DescriptiveColor.lerpColors(
+                (int)((System.currentTimeMillis() >>> 10) * 0x9E377B00 | 0xFE),
+                (int)(((System.currentTimeMillis() >>> 10) + 1L) * 0x9E377B00 | 0xFE),
+                (System.currentTimeMillis() & 0x3FFL) * 0x1p-10f
+                )));
+        batch.draw(font.mapping.get('H'), x += w, y, w, h);
+        batch.draw(font.mapping.get('e'), x += w, y, w, h);
+        batch.draw(font.mapping.get('l'), x += w, y, w, h);
+        batch.draw(font.mapping.get('l'), x += w, y, w, h);
+        batch.draw(font.mapping.get('o'), x += w, y, w, h);
+        batch.draw(font.mapping.get(','), x += w, y, w, h);
+        x += w;
+        batch.draw(font.mapping.get('W'), x += w, y, w, h);
+        batch.draw(font.mapping.get('o'), x += w, y, w, h);
+        batch.draw(font.mapping.get('r'), x += w, y, w, h);
+        batch.draw(font.mapping.get('l'), x += w, y, w, h);
+        batch.draw(font.mapping.get('d'), x += w, y, w, h);
+        batch.draw(font.mapping.get('!'), x += w, y, w, h);
         batch.end();
+        Gdx.graphics.setTitle(Gdx.graphics.getFramesPerSecond() + " FPS");
     }
 }
