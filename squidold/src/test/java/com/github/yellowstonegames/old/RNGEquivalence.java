343814c90840908167b34fa82b220ebbcd779f23
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-04-10 21:59:45 -0700

    Basic requests of longs and ints are unchanged.

---
+++ squidold/src/test/java/com/github/yellowstonegames/old/RNGEquivalence.java
@@ -0,0 +1,29 @@
+package com.github.yellowstonegames.old;
+
+import com.github.yellowstonegames.old.v300.DiverRNG;
+import com.github.yellowstonegames.old.v300.squidmath.RNG;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RNGEquivalence {
+    @Test
+    public void testDiverRNG() {
+        DiverRNG enh = new DiverRNG(0L);
+        com.github.yellowstonegames.old.v300.squidmath.DiverRNG old = new com.github.yellowstonegames.old.v300.squidmath.DiverRNG(0L);
+        RNG rng = new RNG(old);
+        for (int i = 0; i < 20; i++) {
+            Assert.assertEquals(rng.nextLong(), enh.nextLong());
+        }
+        for (int i = 20; i < 200; i++) {
+            Assert.assertEquals(rng.nextLong(i), enh.nextLong(i));
+        }
+
+        for (int i = 0; i < 20; i++) {
+            Assert.assertEquals(rng.nextInt(), enh.nextInt());
+        }
+        for (int i = 20; i < 200; i++) {
+            Assert.assertEquals(rng.nextInt(i), enh.nextInt(i));
+        }
+    }
+
+}
