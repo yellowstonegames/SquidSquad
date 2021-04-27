6a94cd26187d0c5a7064d48ef732e9da7f52a504
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-04-26 22:54:15 -0700

    Explanation and cleanup.

--- squidworld/src/test/java/com/github/yellowstonegames/world/BlueNoiseGenerator.java
+++ squidworld/src/test/java/com/github/yellowstonegames/world/BlueNoiseGenerator.java
@@ -1,7 +1,6 @@
 package com.github.yellowstonegames.world;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Files;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
@@ -18,16 +17,24 @@ import com.github.yellowstonegames.grid.Coord;
 
 import java.io.IOException;
 import java.text.DateFormat;
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Date;
 
 /**
  * Porting Bart Wronski's blue noise generator from NumPy to Java, see
  * https://bartwronski.com/2021/04/21/superfast-void-and-cluster-blue-noise-in-python-numpy-jax/ for more.
+ * <br>
+ * This makes use of some of the more unusual and powerful features in jdkgdxds; that's the reason this tool was moved
+ * from SquidLib (which already has existing code to analyze blue noise, and so seemed like a nice fit) to SquidSquad.
+ * The "energy" level of each pixel in the grid is tracked by an ObjectFloatOrderedMap with Coord keys. This map is
+ * dense, with all 64x64 (or more) cells filled by Coord keys, and the reasons it's used are to pair points with their
+ * energies, and because it is an Ordered, so it provides
+ * {@link com.github.tommyettinger.ds.Ordered#selectRanked(Comparator, int)}. In libGDX, the Select code only applies to
+ * arrays and Arrays (ugh, these names...), but in jdkgdxds, it applies to all Ordered implementations. One could use
+ * OrderedMap's orderedKeys in libGDX and selectRanked() with that, but there's no OrderedMap with primitive float keys.
  */
 public class BlueNoiseGenerator extends ApplicationAdapter {
-    private static final int shift = 6, size = 1 << shift, mask = size - 1;
+    private static final int shift = 7, size = 1 << shift, mask = size - 1;
     private static final double sigma = 1.9, sigma2 = sigma * sigma;
     private final ObjectFloatOrderedMap<Coord> energy = new ObjectFloatOrderedMap<>(size * size);
     private final float[][] lut = new float[size][size];
@@ -35,11 +42,11 @@ public class BlueNoiseGenerator extends ApplicationAdapter {
     private Pixmap pm;
     private LaserRandom rng;
     private PixmapIO.PNG writer;
-    private String date, path;
+    private String path;
 
     @Override
     public void create() {
-        date = DateFormat.getDateInstance().format(new Date());
+        String date = DateFormat.getDateInstance().format(new Date());
         path = "out/blueNoise/" + date + "/";
         
         if(!Gdx.files.local(path).exists())
