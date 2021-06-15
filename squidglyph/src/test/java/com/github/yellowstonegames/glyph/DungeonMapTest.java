61803be91c713289aa84af5ecae1ce4d98450837
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-06-14 22:23:03 -0700

    Show a basic dungeon map as text.

---
+++ squidglyph/src/test/java/com/github/yellowstonegames/glyph/DungeonMapTest.java
@@ -0,0 +1,89 @@
+package com.github.yellowstonegames.glyph;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.github.tommyettinger.ds.support.LaserRandom;
+import com.github.yellowstonegames.core.ArrayTools;
+import com.github.yellowstonegames.core.Hasher;
+import com.github.yellowstonegames.place.DungeonProcessor;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+public class DungeonMapTest extends ApplicationAdapter {
+
+    private SpriteBatch batch;
+    private GlyphMap gm;
+    private DungeonProcessor dungeonProcessor;
+
+    private static final int GRID_WIDTH = 60;
+    private static final int GRID_HEIGHT = 32;
+
+    public static void main(String[] args){
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setTitle("Font test");
+        config.setWindowedMode(GRID_WIDTH * 20, GRID_HEIGHT * 20);
+        config.disableAudio(true);
+        config.useVsync(false);
+        new Lwjgl3Application(new DungeonMapTest(), config);
+    }
+
+    @Override
+    public void create() {
+        LaserRandom random = new LaserRandom(Hasher.decarabia.hash64(DateFormat.getDateInstance().format(new Date())));
+        batch = new SpriteBatch();
+        Font font = KnownFonts.getIosevkaSlab().scaleTo(20f, 20f);
+//        font = KnownFonts.getCascadiaMono().scale(0.5f, 0.5f);
+//        font = KnownFonts.getIosevka().scale(0.75f, 0.75f);
+//        font = KnownFonts.getIosevkaSlab().scale(0.75f, 0.75f);
+//        font = KnownFonts.getDejaVuSansMono().scale(0.75f, 0.75f);
+//        font = KnownFonts.getCozette();
+//        font = KnownFonts.getAStarry();
+        gm = new GlyphMap(font, 60, 32);
+
+        dungeonProcessor = new DungeonProcessor(60, 32, random);
+        regenerate();
+    }
+
+    public void regenerate(){
+        final char[][] dungeon = dungeonProcessor.generate();
+        gm.backgrounds = ArrayTools.fill(0x222222FF, GRID_WIDTH, GRID_HEIGHT);
+        for (int y = 0; y < GRID_HEIGHT; y++) {
+            for (int x = 0; x < GRID_WIDTH; x++) {
+                gm.put(x, y, 0xCCCCCCFF00000000L | dungeon[x][y]);
+            }
+        }
+
+    }
+
+    @Override
+    public void render() {
+        if(Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE))
+            Gdx.app.exit();
+        if(Gdx.input.isKeyJustPressed(Input.Keys.ANY_KEY))
+            regenerate();
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Camera camera = gm.viewport.getCamera();
+        camera.position.set(gm.gridWidth * 0.5f, gm.gridHeight * 0.5f, 0f);
+        camera.update();
+        gm.viewport.apply(false);
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        gm.draw(batch, 0, 0);
+        batch.end();
+        Gdx.graphics.setTitle(Gdx.graphics.getFramesPerSecond() + " FPS");
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+        gm.resize(width, height);
+    }
+}
