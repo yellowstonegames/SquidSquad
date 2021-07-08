597619d163a83a2064f1800b57bc23558c72f1ff
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-07-07 22:28:06 -0700

    Start setting up SquidStoreGrid.

---
+++ squidstore/grid/src/main/java/com/github/yellowstonegames/store/grid/JsonGrid.java
@@ -0,0 +1,27 @@
+package com.github.yellowstonegames.store.grid;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.utils.Json;
+import com.badlogic.gdx.utils.JsonValue;
+import com.badlogic.gdx.utils.reflect.ClassReflection;
+import com.badlogic.gdx.utils.reflect.ReflectionException;
+import com.github.tommyettinger.ds.ObjectList;
+import com.github.tommyettinger.ds.interop.JsonSupport;
+import com.github.tommyettinger.ds.support.*;
+import com.github.yellowstonegames.core.*;
+
+import javax.annotation.Nonnull;
+
+@SuppressWarnings("rawtypes")
+public final class JsonGrid {
+    private JsonGrid() {
+    }
+
+    /**
+     * Registers SquidGrid's classes with the given Json object, allowing it to read and write SquidGrid types.
+     *
+     * @param json a libGDX Json object that will have serializers registered for all SquidGrid types.
+     */
+    public static void registerAll(@Nonnull Json json) {
+    }
+}
---
+++ squidstore/grid/src/test/java/com/github/yellowstonegames/store/grid/JsonGridTest.java
@@ -0,0 +1,19 @@
+package com.github.yellowstonegames.store.grid;
+
+import com.badlogic.gdx.utils.Json;
+import com.badlogic.gdx.utils.JsonWriter;
+import com.github.tommyettinger.ds.interop.JsonSupport;
+import com.github.tommyettinger.ds.support.DistinctRandom;
+import com.github.tommyettinger.ds.support.FourWheelRandom;
+import com.github.tommyettinger.ds.support.LaserRandom;
+import com.github.tommyettinger.ds.support.TricycleRandom;
+import com.github.yellowstonegames.core.ArrayTools;
+import com.github.yellowstonegames.core.Dice;
+import com.github.yellowstonegames.core.GapShuffler;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class JsonGridTest {
+}
