36b55d53c4b5d2112eef1277f4cd92d3e23391b2
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-07-08 19:44:30 -0700

    Add Coord ser/deser to JsonGrid.

--- squidstore/grid/src/main/java/com/github/yellowstonegames/store/grid/JsonGrid.java
+++ squidstore/grid/src/main/java/com/github/yellowstonegames/store/grid/JsonGrid.java
@@ -9,6 +9,8 @@ import com.github.tommyettinger.ds.ObjectList;
 import com.github.tommyettinger.ds.interop.JsonSupport;
 import com.github.tommyettinger.ds.support.*;
 import com.github.yellowstonegames.core.*;
+import com.github.yellowstonegames.grid.Coord;
+import com.github.yellowstonegames.store.core.JsonCore;
 
 import javax.annotation.Nonnull;
 
@@ -23,5 +25,30 @@ public final class JsonGrid {
      * @param json a libGDX Json object that will have serializers registered for all SquidGrid types.
      */
     public static void registerAll(@Nonnull Json json) {
+        registerCoord(json);
     }
+
+    /**
+     * Registers Coord with the given Json object, so Coord can be written to and read from JSON.
+     *
+     * @param json a libGDX Json object that will have a serializer registered
+     */
+    public static void registerCoord(@Nonnull Json json) {
+        json.setSerializer(Coord.class, new Json.Serializer<Coord>() {
+            @Override
+            public void write(Json json, Coord object, Class knownType) {
+                json.writeArrayStart();
+                json.writeValue(object.x);
+                json.writeValue(object.y);
+                json.writeArrayEnd();
+            }
+
+            @Override
+            public Coord read(Json json, JsonValue jsonData, Class type) {
+                if (jsonData == null || jsonData.isNull()) return null;
+                return Coord.get(jsonData.getInt(0), jsonData.getInt(1));
+            }
+        });
+    }
+
 }
