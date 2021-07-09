78e332f328040f347df615d62a881caa46d50b8b
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-07-08 20:02:02 -0700

    Tests for Region ser/deser.

--- squidstore/grid/src/test/java/com/github/yellowstonegames/store/grid/JsonGridTest.java
+++ squidstore/grid/src/test/java/com/github/yellowstonegames/store/grid/JsonGridTest.java
@@ -11,6 +11,7 @@ import com.github.yellowstonegames.core.ArrayTools;
 import com.github.yellowstonegames.core.Dice;
 import com.github.yellowstonegames.core.GapShuffler;
 import com.github.yellowstonegames.grid.Coord;
+import com.github.yellowstonegames.grid.Region;
 import com.github.yellowstonegames.store.core.JsonCore;
 import org.junit.Assert;
 import org.junit.Test;
@@ -47,12 +48,44 @@ public class JsonGridTest {
         System.out.println(data);
         coord2 = json.fromJson(Coord.class, data);
         Assert.assertEquals(coord, coord2);
-        
+
         coord = Coord.get(-20, -20);
         data = json.toJson(coord);
         System.out.println(data);
         coord2 = json.fromJson(Coord.class, data);
         Assert.assertEquals(coord, coord2);
+
+        System.out.println();
+    }
+
+    @Test
+    public void testRegion() {
+        Json json = new Json(JsonWriter.OutputType.minimal);
+        JsonGrid.registerRegion(json);
+        Region region, region2;
+        region = new Region(Coord.get(3, 3), 8, 8);
+        String data = json.toJson(region);
+        System.out.println(data);
+        region2 = json.fromJson(Region.class, data);
+        Assert.assertEquals(region, region2);
+
+        region.not();
+        data = json.toJson(region);
+        System.out.println(data);
+        region2 = json.fromJson(Region.class, data);
+        Assert.assertEquals(region, region2);
+
+        region.retract(2);
+        data = json.toJson(region);
+        System.out.println(data);
+        region2 = json.fromJson(Region.class, data);
+        Assert.assertEquals(region, region2);
+
+        region.not().expand8way();
+        data = json.toJson(region);
+        System.out.println(data);
+        region2 = json.fromJson(Region.class, data);
+        Assert.assertEquals(region, region2);
 
         System.out.println();
     }
