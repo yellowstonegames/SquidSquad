824ae6b7c32e8b086d4dfaecb6bff5156d344ba3
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-07-16 22:03:00 -0700

    JsonOldTest: DiverRNG.

--- squidstore/old/src/test/java/com/github/yellowstonegames/store/old/JsonOldTest.java
+++ squidstore/old/src/test/java/com/github/yellowstonegames/store/old/JsonOldTest.java
@@ -2,13 +2,14 @@ package com.github.yellowstonegames.store.old;
 
 import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.JsonWriter;
+import com.github.yellowstonegames.old.v300.DiverRNG;
 import com.github.yellowstonegames.old.v300.LightRNG;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class JsonOldTest {
     @Test
-    public void testDistinctRandom() {
+    public void testLightRNG() {
         Json json = new Json(JsonWriter.OutputType.minimal);
         JsonOld.registerLightRNG(json);
         LightRNG random = new LightRNG(123456789);
@@ -16,6 +17,19 @@ public class JsonOldTest {
         String data = json.toJson(random);
         System.out.println(data);
         LightRNG random2 = json.fromJson(LightRNG.class, data);
+        System.out.println(Long.toString(random2.getSelectedState(0), 36));
+        Assert.assertEquals(random.nextLong(), random2.nextLong());
+    }
+
+    @Test
+    public void testDiverRNG() {
+        Json json = new Json(JsonWriter.OutputType.minimal);
+        JsonOld.registerDiverRNG(json);
+        DiverRNG random = new DiverRNG(123456789);
+        random.nextLong();
+        String data = json.toJson(random);
+        System.out.println(data);
+        DiverRNG random2 = json.fromJson(DiverRNG.class, data);
         System.out.println(Long.toString(random2.getSelectedState(0), 36));
         Assert.assertEquals(random.nextLong(), random2.nextLong());
     }
