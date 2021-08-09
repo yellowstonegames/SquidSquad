ae00cbb29ca94a8882da1a413bc26586244f77e1
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-08-08 20:00:35 -0700

    Finally got the tri mapping right (32x32 for now).

--- squidworld/src/test/java/com/github/yellowstonegames/world/BlueNoiseEqualOmniTilingGenerator.java
+++ squidworld/src/test/java/com/github/yellowstonegames/world/BlueNoiseEqualOmniTilingGenerator.java
@@ -69,11 +69,11 @@ public class BlueNoiseEqualOmniTilingGenerator extends ApplicationAdapter {
     /**
      * Affects the size of the parent noise; typically 8 or 9 for a 256x256 or 5125x512 parent image.
      */
-    private static final int shift = 9;
+    private static final int shift = 8;
     /**
      * Affects how many sectors are cut out of the full size; this is an exponent (with a base of 2).
      */
-    private static final int sectorShift = 2;
+    private static final int sectorShift = 3;
 
     private static final int size = 1 << shift;
     private static final int sectors = 1 << sectorShift;
@@ -83,7 +83,7 @@ public class BlueNoiseEqualOmniTilingGenerator extends ApplicationAdapter {
     private static final int sectorMask = sector - 1;
     private static final int wrapMask = sectorMask >>> 1;
     private static final float fraction = 1f / totalSectors;
-    private static final int lightOccurrence = size * size >>> 8 + sectorShift + sectorShift;
+    private static final int lightOccurrence = 1;//size * size >>> 8 + sectorShift + sectorShift;
 
     private static final double sigma = 1.9, sigma2 = sigma * sigma;
     private final ObjectFloatOrderedMap<Coord> energy = new ObjectFloatOrderedMap<Coord>(size * size, 0.5f)
@@ -229,20 +229,25 @@ public class BlueNoiseEqualOmniTilingGenerator extends ApplicationAdapter {
             done[low.x][low.y] = ctr;
         }
         ByteBuffer buffer = pm.getPixels();
-        final int toByteShift = Math.max(0, shift + shift - 8);
         if(isTriangular) {
+            final int toKindaByteShift = Math.max(0, shift + shift - 8 - 2);
             for (int x = 0; x < size; x++) {
                 for (int y = 0; y < size; y++) {
-                    float rnd = (done[x][y] >>> toByteShift) * 0x1p-8f + 0.5f;
-                    rnd -= (int) rnd;
-                    float orig = rnd * 2f - 1f;
-                    rnd = (orig == 0f) ? 0f : (float) (orig / Math.sqrt(Math.abs(orig)));
-                    rnd = (rnd - Math.signum(orig)) * 127.5f + 127.5f;
-                    buffer.putInt((Math.round(rnd) & 0xFF) * 0x01010100 | 0xFF);
+                    float r = (done[x][y] >>> toKindaByteShift) * (1f / ((1 << 8 + 2) - 1));
+                    // old tri map
+//                    r += 0.5f;
+//                    r -= (int) r;
+//                    float orig = r * 2f - 1f;
+//                    r = (orig == 0f) ? 0f : (float) (orig / Math.sqrt(Math.abs(orig)));
+//                    r = (r - Math.signum(orig)) * 127.5f + 127.5f;
+                    // new tri map
+                    r = ((r > 0.5f) ? 1f - (float)Math.sqrt(2f - 2f * r) : (float)Math.sqrt(2f * r) - 1f) * 127.5f + 127.5f;
+                    buffer.putInt((Math.round(r) & 0xFF) * 0x01010100 | 0xFF);
                 }
             }
         }
         else {
+            final int toByteShift = Math.max(0, shift + shift - 8);
             for (int x = 0; x < size; x++) {
                 for (int y = 0; y < size; y++) {
                     buffer.putInt((done[x][y] >>> toByteShift) * 0x01010100 | 0xFF);
