2a643412c6d448f0c472e8953e2c98dcf708a592
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-08-25 20:49:55 -0700

    Add ExponentialDistribution .

--- squidcore/src/main/java/com/github/yellowstonegames/core/IDistribution.java
+++ squidcore/src/main/java/com/github/yellowstonegames/core/IDistribution.java
@@ -199,5 +199,54 @@ public interface IDistribution {
             return sum * i_degree;
         }
     }
+    /**
+     * An IDistribution that implements the <a href="https://en.wikipedia.org/wiki/Exponential_distribution">Exponential
+     * distribution</a>. Takes lambda as a parameter during construction (default 1), and lambda also has
+     * getters/setters. Note that lambda is just the parameter name, and doesn't have anything to do with Java lambdas.
+     */
+    class ExponentialDistribution implements IDistribution {
+        public static final ExponentialDistribution instance = new ExponentialDistribution();
+        public static final ExponentialDistribution instance_0_5 = new ExponentialDistribution(0.5);
+        public static final ExponentialDistribution instance_1_5 = new ExponentialDistribution(1.5);
+        private double i_lambda;
+        public ExponentialDistribution()
+        {
+            i_lambda = 1.0;
+        }
+        public ExponentialDistribution(double lambda)
+        {
+            i_lambda = 1.0 / lambda;
+        }
+
+        public double getLambda() {
+            return 1.0 / i_lambda;
+        }
+
+        public void setLambda(double lambda) {
+            this.i_lambda = 1.0 / lambda;
+        }
+
+        @Override
+        public double nextDouble(EnhancedRandom rng) {
+            return Math.log(rng.nextExclusiveDouble()) * i_lambda;
+        }
+        /**
+         * The lower exclusive bound is 0 while lambda is positive; it is negative infinity if lambda is negative.
+         * @return zero, or negative infinity if lambda is negative.
+         */
+        @Override
+        public double getLowerBound() {
+            return i_lambda < 0.0 ? Double.NEGATIVE_INFINITY : 0.0;
+        }
+
+        /**
+         * The upper exclusive bound is infinity while lambda is positive; it is 0 if lambda is negative.
+         * @return positive infinity, or zero if lambda is negative.
+         */
+        @Override
+        public double getUpperBound() {
+            return i_lambda < 0.0 ? 0.0 : Double.POSITIVE_INFINITY;
+        }
+    }
 
 }
