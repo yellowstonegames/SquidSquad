e807a5e988d3d1f8d759e8de7e1cfcd56aa20cdf
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-08-25 20:44:44 -0700

    Add CurvedBoundedDistribution .

--- squidcore/src/main/java/com/github/yellowstonegames/core/IDistribution.java
+++ squidcore/src/main/java/com/github/yellowstonegames/core/IDistribution.java
@@ -159,5 +159,45 @@ public interface IDistribution {
             return d - (int)d;
         }
     }
+    /**
+     * An IDistribution that allows a parameter to determine how many calls to {@link EnhancedRandom#nextDouble()} to make and average
+     * whenever a double is requested. When this parameter {@code degree} is 1, this is uniform; when it is 2, this is a
+     * triangular distribution, and when it is 3 or more it is an increasingly centralized bell curve. The average value is
+     * always very close to 0.5, and the bounds are inclusive on 0.0, exclusive on 1.0 .
+     */
+    class CurvedBoundedDistribution extends IDistribution.SimpleDistribution implements IDistribution {
+        public static final CurvedBoundedDistribution instance = new CurvedBoundedDistribution(3);
+        public static final CurvedBoundedDistribution instanceTriangular = new CurvedBoundedDistribution(2);
+        public static final CurvedBoundedDistribution instanceGaussianLike = new CurvedBoundedDistribution(6);
+        private int degree;
+        private double i_degree;
+        public CurvedBoundedDistribution()
+        {
+            this(3);
+        }
+        public CurvedBoundedDistribution(int degree)
+        {
+            this.degree = Math.max(degree, 1);
+            i_degree = 1.0 / this.degree;
+        }
+
+        public int getDegree() {
+            return degree;
+        }
+
+        public void setDegree(int degree) {
+            this.degree = Math.max(degree, 1);
+            i_degree = 1.0 / this.degree;
+        }
+
+        @Override
+        public double nextDouble(EnhancedRandom rng) {
+            double sum = 0.0;
+            for (int i = 0; i < degree; i++) {
+                sum += rng.nextDouble();
+            }
+            return sum * i_degree;
+        }
+    }
 
 }
