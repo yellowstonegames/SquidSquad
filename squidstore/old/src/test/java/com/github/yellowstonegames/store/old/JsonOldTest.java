7cba5eabd15f9051045b72d5eebcb40b95e4efdd
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-08-27 22:05:26 -0700

    Add tests for GWTRNG and SilkRNG storage.

--- squidstore/old/src/test/java/com/github/yellowstonegames/store/old/JsonOldTest.java
+++ squidstore/old/src/test/java/com/github/yellowstonegames/store/old/JsonOldTest.java
@@ -3,7 +3,9 @@ package com.github.yellowstonegames.store.old;
 import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.JsonWriter;
 import com.github.yellowstonegames.old.v300.DiverRNG;
+import com.github.yellowstonegames.old.v300.GWTRNG;
 import com.github.yellowstonegames.old.v300.LightRNG;
+import com.github.yellowstonegames.old.v300.SilkRNG;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -34,4 +36,29 @@ public class JsonOldTest {
         Assert.assertEquals(random.nextLong(), random2.nextLong());
     }
 
+    @Test
+    public void testGWTRNG() {
+        Json json = new Json(JsonWriter.OutputType.minimal);
+        JsonOld.registerGWTRNG(json);
+        GWTRNG random = new GWTRNG(123456789);
+        random.nextLong();
+        String data = json.toJson(random);
+        System.out.println(data);
+        GWTRNG random2 = json.fromJson(GWTRNG.class, data);
+        System.out.println(Long.toString(random2.getSelectedState(0), 36));
+        Assert.assertEquals(random.nextLong(), random2.nextLong());
+    }
+
+    @Test
+    public void testSilkRNG() {
+        Json json = new Json(JsonWriter.OutputType.minimal);
+        JsonOld.registerSilkRNG(json);
+        SilkRNG random = new SilkRNG(123456789);
+        random.nextLong();
+        String data = json.toJson(random);
+        System.out.println(data);
+        SilkRNG random2 = json.fromJson(SilkRNG.class, data);
+        System.out.println(Long.toString(random2.getSelectedState(0), 36));
+        Assert.assertEquals(random.nextLong(), random2.nextLong());
+    }
 }
