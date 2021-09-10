54589c95fa4ce602a4b8a95718232a5860037377
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-09-09 20:40:18 -0700

    And there's the rest of FOV/seen tracking.

--- squidglyph/src/test/java/com/github/yellowstonegames/glyph/DungeonMapTest.java
+++ squidglyph/src/test/java/com/github/yellowstonegames/glyph/DungeonMapTest.java
@@ -2,7 +2,6 @@ package com.github.yellowstonegames.glyph;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
@@ -31,6 +30,7 @@ public class DungeonMapTest extends ApplicationAdapter {
     private DungeonProcessor dungeonProcessor;
     private char[][] bare;
     private float[][] res, light;
+    private Region seen, inView;
     private final Noise waves = new Noise(123, 0.5f, Noise.FOAM, 1);
     private Director<GlidingGlyph> director;
     private ObjectList<GlidingGlyph> glyphs;
@@ -67,17 +67,18 @@ public class DungeonMapTest extends ApplicationAdapter {
 //        font = KnownFonts.getCozette();
 //        font = KnownFonts.getAStarry();
         gm = new GlyphMap(font, 60, 32);
-        glyphs = new ObjectList<>(1);
+        glyphs = ObjectList.with(new GlidingGlyph((long) describe("red orange") << 32 | '@', Coord.get(1, 1)));
         director = new Director<>(GlidingGlyph::getLocation, glyphs);
         director.setDuration(100L);
         dungeonProcessor = new DungeonProcessor(60, 32, random);
         dungeonProcessor.addWater(DungeonProcessor.ALL, 40);
         waves.setFractalType(Noise.RIDGED_MULTI);
         light = new float[GRID_WIDTH][GRID_HEIGHT];
+        seen = new Region(GRID_WIDTH, GRID_HEIGHT);
+        inView = new Region(GRID_WIDTH, GRID_HEIGHT);
         Gdx.input.setInputProcessor(new InputAdapter(){
             @Override
             public boolean keyDown(int keycode) {
-                CoordGlider cg = glyphs.first().getLocation();
                 Coord next;
                 switch (keycode){
                     case ESCAPE:
@@ -89,35 +90,19 @@ public class DungeonMapTest extends ApplicationAdapter {
                         break;
                     case W:
                     case UP:
-                        next = cg.getStart().translate(Direction.UP);
-                        if(next.isWithin(GRID_WIDTH, GRID_HEIGHT) && bare[next.x][next.y] == '.') {
-                            cg.setEnd(next);
-                            director.play();
-                        }
+                        move(Direction.UP);
                         break;
                     case A:
                     case LEFT:
-                        next = cg.getStart().translate(Direction.LEFT);
-                        if(next.isWithin(GRID_WIDTH, GRID_HEIGHT) && bare[next.x][next.y] == '.') {
-                            cg.setEnd(next);
-                            director.play();
-                        }
+                        move(Direction.LEFT);
                         break;
                     case S:
                     case DOWN:
-                        next = cg.getStart().translate(Direction.DOWN);
-                        if(next.isWithin(GRID_WIDTH, GRID_HEIGHT) && bare[next.x][next.y] == '.') {
-                            cg.setEnd(next);
-                            director.play();
-                        }
+                        move(Direction.DOWN);
                         break;
                     case D:
                     case RIGHT:
-                        next = cg.getStart().translate(Direction.RIGHT);
-                        if(next.isWithin(GRID_WIDTH, GRID_HEIGHT) && bare[next.x][next.y] == '.') {
-                            cg.setEnd(next);
-                            director.play();
-                        }
+                        move(Direction.RIGHT);
                         break;
                     default: return false;
                 }
@@ -128,13 +113,25 @@ public class DungeonMapTest extends ApplicationAdapter {
         regenerate();
     }
 
+    public void move(Direction way){
+        final CoordGlider cg = glyphs.first().getLocation();
+        final Coord next = cg.getStart().translate(way);
+        if(next.isWithin(GRID_WIDTH, GRID_HEIGHT) && bare[next.x][next.y] == '.') {
+            cg.setEnd(next);
+            cg.setCompleteRunner(() ->
+                    seen.or(inView.refill(FOV.reuseFOV(res, light, cg.getEnd().x, cg.getEnd().y, 6.5f, Radius.CIRCLE), 0.001f, 2f)));
+            director.play();
+        }
+    }
+
     public void regenerate(){
         dungeonProcessor.setPlaceGrid(LineTools.hashesToLines(dungeonProcessor.generate(), true));
         bare = dungeonProcessor.getBarePlaceGrid();
         res = FOV.generateSimpleResistances(bare);
         Coord player = new Region(bare, '.').singleRandom(dungeonProcessor.rng);
-        glyphs.clear();
-        glyphs.add(new GlidingGlyph((long) describe("red orange") << 32 | '@', player));
+        glyphs.first().getLocation().setStart(player);
+        glyphs.first().getLocation().setEnd(player);
+        seen.or(inView.refill(FOV.reuseFOV(res, light, player.x, player.y, 6.5f, Radius.CIRCLE), 0.001f, 2f));
         gm.backgrounds = new int[GRID_WIDTH][GRID_HEIGHT];
         gm.map.clear();
     }
@@ -146,21 +143,44 @@ public class DungeonMapTest extends ApplicationAdapter {
         FOV.reuseFOV(res, light, player.x, player.y, swayRandomized(dungeonProcessor.rng.getStateA(), modifiedTime) * 2.5f + 4f, Radius.CIRCLE);
         for (int y = 0; y < GRID_HEIGHT; y++) {
             for (int x = 0; x < GRID_WIDTH; x++) {
-                switch (dungeon[x][y]){
-                    case '~':
-                        gm.backgrounds[x][y] = toRGBA8888(lighten(DEEP_OKLAB, 0.6f * Math.max(0, light[x][y] + waves.getConfiguredNoise(x, y, modifiedTime))));
-                        gm.put(x, y, deepText << 32 | dungeon[x][y]);
-                        break;
-                    case ',':
-                        gm.backgrounds[x][y] = toRGBA8888(lighten(SHALLOW_OKLAB, 0.6f * Math.max(0, light[x][y] + waves.getConfiguredNoise(x, y, modifiedTime))));
-                        gm.put(x, y, shallowText << 32 | dungeon[x][y]);
-                        break;
-                    case ' ':
-                        gm.backgrounds[x][y] = 0;
-                        break;
-                    default:
-                        gm.backgrounds[x][y] = toRGBA8888(lighten(STONE_OKLAB, 0.6f * light[x][y]));
-                        gm.put(x, y, stoneText << 32 | dungeon[x][y]);
+                if(inView.contains(x, y)) {
+                    switch (dungeon[x][y]) {
+                        case '~':
+                            gm.backgrounds[x][y] = toRGBA8888(lighten(DEEP_OKLAB, 0.6f * Math.max(0, light[x][y] + waves.getConfiguredNoise(x, y, modifiedTime))));
+                            gm.put(x, y, deepText << 32 | dungeon[x][y]);
+                            break;
+                        case ',':
+                            gm.backgrounds[x][y] = toRGBA8888(lighten(SHALLOW_OKLAB, 0.6f * Math.max(0, light[x][y] + waves.getConfiguredNoise(x, y, modifiedTime))));
+                            gm.put(x, y, shallowText << 32 | dungeon[x][y]);
+                            break;
+                        case ' ':
+                            gm.backgrounds[x][y] = 0;
+                            break;
+                        default:
+                            gm.backgrounds[x][y] = toRGBA8888(lighten(STONE_OKLAB, 0.6f * light[x][y]));
+                            gm.put(x, y, stoneText << 32 | dungeon[x][y]);
+                    }
+                }
+                else if(seen.contains(x, y)){
+                    switch (dungeon[x][y]) {
+                        case '~':
+                            gm.backgrounds[x][y] = toRGBA8888(edit(DEEP_OKLAB, 0f, 0f, 0f, 0f, 0.7f, 0f, 0f, 1f));
+                            gm.put(x, y, deepText << 32 | dungeon[x][y]);
+                            break;
+                        case ',':
+                            gm.backgrounds[x][y] = toRGBA8888(edit(SHALLOW_OKLAB, 0f, 0f, 0f, 0f, 0.7f, 0f, 0f, 1f));
+                            gm.put(x, y, shallowText << 32 | dungeon[x][y]);
+                            break;
+                        case ' ':
+                            gm.backgrounds[x][y] = 0;
+                            break;
+                        default:
+                            gm.backgrounds[x][y] = toRGBA8888(edit(STONE_OKLAB, 0f, 0f, 0f, 0f, 0.7f, 0f, 0f, 1f));
+                            gm.put(x, y, stoneText << 32 | dungeon[x][y]);
+                    }
+                }
+                else {
+                    gm.backgrounds[x][y] = 0;
                 }
             }
         }
