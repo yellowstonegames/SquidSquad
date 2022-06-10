a9c442b295b6138e5776e6af1ddb46388cb51dea
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-06-09 21:48:04 -0700

    I feel the need, the need for VDC...

---
+++ squidgrid/src/main/java/com/github/yellowstonegames/grid/QuasiRandomTools.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2022 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.yellowstonegames.grid;
+
+/**
+ * Static methods to produce numbers and points from quasi-random sequences (which seem like random sequences, but have
+ * lower "discrepancy" between results). In practice, this means that the points this produces from a sequence tend to
+ * be distant from other points from nearby in the sequence.
+ */
+public class QuasiRandomTools {
+    /**
+     * Gets the {@code index}-th element from the base-{@code base} van der Corput sequence. The base should usually be
+     * a prime number. The index must be greater than 0 and should be less than 16777216. The number this returns is a
+     * float between 0 (inclusive) and 1 (exclusive).
+     *
+     * @param base  a prime number to use as the base/radix of the van der Corput sequence
+     * @param index the position in the sequence of the requested base, as a positive int
+     * @return a quasi-random float between 0.0 (inclusive) and 1.0 (exclusive).
+     */
+    public static float vanDerCorput(final int base, final int index) {
+        if (base <= 2) {
+            return (Integer.reverse(index) >>> 8) * 0x1p-24f;
+        }
+        float denominator = base, res = 0.0f;
+        int n = (index & 0x00ffffff);
+        while (n > 0) {
+            res += (n % base) / denominator;
+            n /= base;
+            denominator *= base;
+        }
+        return res;
+    }
+
+}
