cee33abe0ec7ef54b799405ef01f9b18718c2bd8
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-12-16 19:39:36 -0800

    CyclicNoise reads/writes well now.

--- squidfreeze/grid/src/test/java/com/github/yellowstonegames/freeze/grid/GridTest.java
+++ squidfreeze/grid/src/test/java/com/github/yellowstonegames/freeze/grid/GridTest.java
@@ -354,4 +354,22 @@ public class GridTest {
             Assert.assertEquals(data, data2);
         }
     }
+
+    @Test
+    public void testCyclicNoise() {
+        Kryo kryo = new Kryo();
+        kryo.register(CyclicNoise.class, new CyclicNoiseSerializer());
+
+        CyclicNoise data = new CyclicNoise(-9876543210L, 8);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream(32);
+        Output output = new Output(baos);
+        kryo.writeObject(output, data);
+        byte[] bytes = output.toBytes();
+        try (Input input = new Input(bytes)) {
+            CyclicNoise data2 = kryo.readObject(input, CyclicNoise.class);
+            Assert.assertEquals(data.getNoise(0.1f, 0.2f, 0.3f), data2.getNoise(0.1f, 0.2f, 0.3f), Float.MIN_NORMAL);
+            Assert.assertEquals(data, data2);
+        }
+    }
 }
