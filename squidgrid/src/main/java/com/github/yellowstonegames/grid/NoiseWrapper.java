93c3b9100a0b2b72b5e77e5d2ed79a60178dd41d
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-12-20 15:41:28 -0800

    6D NoiseWrapper.
    Soon we'll need to test it and add ser/deser for NoiseWrapper.

--- squidgrid/src/main/java/com/github/yellowstonegames/grid/NoiseWrapper.java
+++ squidgrid/src/main/java/com/github/yellowstonegames/grid/NoiseWrapper.java
@@ -57,34 +57,56 @@ public class NoiseWrapper implements INoise {
         this.frequency = frequency;
     }
 
+    /**
+     * Wraps {@link #getFractalType()}.
+     * @return an int between 0 and 3, corresponding to {@link Noise#FBM}, {@link Noise#BILLOW}, {@link Noise#RIDGED_MULTI}, or {@link Noise#DOMAIN_WARP}
+     */
     public int getMode() {
-        return mode;
+        return getFractalType();
     }
 
+    /**
+     * Wra[s {@link #setFractalType(int)}
+     * @param mode an int between 0 and 3, corresponding to {@link Noise#FBM}, {@link Noise#BILLOW}, {@link Noise#RIDGED_MULTI}, or {@link Noise#DOMAIN_WARP}
+     */
     public void setMode(int mode) {
-        this.mode = mode;
+        setFractalType(mode);
     }
 
     public int getFractalType() {
         return mode;
     }
 
+    /**
+     * @param mode an int between 0 and 3, corresponding to {@link Noise#FBM}, {@link Noise#BILLOW}, {@link Noise#RIDGED_MULTI}, or {@link Noise#DOMAIN_WARP}
+     */
     public void setFractalType(int mode) {
         this.mode = mode;
     }
 
+    /**
+     * Wraps {@link #getFractalOctaves()}.
+     * @return how many octaves this uses to increase detail
+     */
     public int getOctaves() {
-        return octaves;
+        return getFractalOctaves();
     }
 
+    /**
+     * Wraps {@link #setFractalOctaves(int)}.
+     * @param octaves how many octaves to use to increase detail; must be at least 1.
+     */
     public void setOctaves(int octaves) {
-        this.octaves = Math.max(1, octaves);
+        setFractalOctaves(octaves);
     }
 
     public int getFractalOctaves() {
         return octaves;
     }
 
+    /**
+     * @param octaves how many octaves to use to increase detail; must be at least 1.
+     */
     public void setFractalOctaves(int octaves) {
         this.octaves = Math.max(1, octaves);
     }
@@ -154,7 +176,12 @@ public class NoiseWrapper implements INoise {
 
     @Override
     public float getNoise(float x, float y, float z, float w, float u, float v) {
-        return wrapped.getNoise(x, y, z, w, u, v);
+        switch (mode) {
+            default: return fbm(x, y, z, w, u, v, seed);
+            case 1: return billow(x, y, z, w, u, v, seed);
+            case 2: return ridged(x, y, z, w, u, v, seed);
+            case 3: return warp(x, y, z, w, u, v, seed);
+        }
     }
 
     @Override
@@ -215,7 +242,13 @@ public class NoiseWrapper implements INoise {
 
     @Override
     public float getNoiseWithSeed(float x, float y, float z, float w, float u, float v, long seed) {
-        return wrapped.getNoiseWithSeed(x, y, z, w, u, v, seed);
+        switch (mode) {
+            default:
+            case 0: return fbm(x * frequency, y * frequency, z * frequency, w * frequency, u * frequency, v * frequency, seed);
+            case 1: return billow(x * frequency, y * frequency, z * frequency, w * frequency, u * frequency, v * frequency, seed);
+            case 2: return ridged(x * frequency, y * frequency, z * frequency, w * frequency, u * frequency, v * frequency, seed);
+            case 3: return warp(x * frequency, y * frequency, z * frequency, w * frequency, u * frequency, v * frequency, seed);
+        }
     }
 
     // 2D
@@ -598,6 +631,125 @@ public class NoiseWrapper implements INoise {
 
             amp *= 0.5f;
             sum += (latest = wrapped.getNoiseWithSeed(x + a, y + b, z + c, w + d, u + e, seed + i)) * amp;
+        }
+
+        return sum * ((1 << octaves) - 1);
+    }
+
+    // 5D
+
+    protected float fbm(float x, float y, float z, float w, float u, float v, long seed) {
+        float sum = wrapped.getNoiseWithSeed(x, y, z, w, u, v, seed);
+        float amp = 1;
+
+        for (int i = 1; i < octaves; i++) {
+            if(fractalSpiral){
+                final float x2 = rotateX6D(x, y, z, w, u, v);
+                final float y2 = rotateY6D(x, y, z, w, u, v);
+                final float z2 = rotateZ6D(x, y, z, w, u, v);
+                final float w2 = rotateW6D(x, y, z, w, u, v);
+                final float u2 = rotateU6D(x, y, z, w, u, v);
+                final float v2 = rotateV6D(x, y, z, w, u, v);
+                x = x2; y = y2; z = z2; w = w2; u = u2; v = v2;
+            }
+            x *= 2f;
+            y *= 2f;
+            z *= 2f;
+            w *= 2f;
+            u *= 2f;
+            v *= 2f;
+
+            amp *= 0.5f;
+            sum += wrapped.getNoiseWithSeed(x, y, z, w, u, v, seed + i) * amp;
+        }
+
+        return sum / ((1 << octaves) - 1);
+    }
+    protected float billow(float x, float y, float z, float w, float u, float v, long seed) {
+        float sum = Math.abs(wrapped.getNoiseWithSeed(x, y, z, w, u, v, seed)) * 2 - 1;
+        float amp = 1;
+
+        for (int i = 1; i < octaves; i++) {
+            if(fractalSpiral){
+                final float x2 = rotateX6D(x, y, z, w, u, v);
+                final float y2 = rotateY6D(x, y, z, w, u, v);
+                final float z2 = rotateZ6D(x, y, z, w, u, v);
+                final float w2 = rotateW6D(x, y, z, w, u, v);
+                final float u2 = rotateU6D(x, y, z, w, u, v);
+                final float v2 = rotateV6D(x, y, z, w, u, v);
+                x = x2; y = y2; z = z2; w = w2; u = u2; v = v2;
+            }
+            x *= 2f;
+            y *= 2f;
+            z *= 2f;
+            w *= 2f;
+            u *= 2f;
+            v *= 2f;
+
+            amp *= 0.5f;
+            sum += (Math.abs(wrapped.getNoiseWithSeed(x, y, z, w, u, v, seed + i)) * 2 - 1) * amp;
+        }
+
+        return sum * ((1 << octaves) - 1);
+    }
+
+    protected float ridged(float x, float y, float z, float w, float u, float v, long seed) {
+        float sum = 0f, exp = 2f, correction = 0f, spike;
+        for (int i = 0; i < octaves; i++) {
+            spike = 1f - Math.abs(wrapped.getNoiseWithSeed(x, y, z, w, u, v, seed + i));
+            correction += (exp *= 0.5f);
+            sum += spike * exp;
+            if(fractalSpiral){
+                final float x2 = rotateX6D(x, y, z, w, u, v);
+                final float y2 = rotateY6D(x, y, z, w, u, v);
+                final float z2 = rotateZ6D(x, y, z, w, u, v);
+                final float w2 = rotateW6D(x, y, z, w, u, v);
+                final float u2 = rotateU6D(x, y, z, w, u, v);
+                final float v2 = rotateV6D(x, y, z, w, u, v);
+                x = x2; y = y2; z = z2; w = w2; u = u2; v = v2;
+            }
+            x *= 2f;
+            y *= 2f;
+            z *= 2f;
+            w *= 2f;
+            u *= 2f;
+            v *= 2f;
+        }
+        return sum * 2f / correction - 1f;
+    }
+
+    protected float warp(float x, float y, float z, float w, float u, float v, long seed) {
+        float latest = wrapped.getNoiseWithSeed(x, y, z, w, u, v, seed);
+        float sum = latest;
+        float amp = 1;
+
+        for (int i = 1; i < octaves; i++) {
+            if(fractalSpiral){
+                final float x2 = rotateX6D(x, y, z, w, u, v);
+                final float y2 = rotateY6D(x, y, z, w, u, v);
+                final float z2 = rotateZ6D(x, y, z, w, u, v);
+                final float w2 = rotateW6D(x, y, z, w, u, v);
+                final float u2 = rotateU6D(x, y, z, w, u, v);
+                final float v2 = rotateV6D(x, y, z, w, u, v);
+                x = x2; y = y2; z = z2; w = w2; u = u2; v = v2;
+            }
+            x *= 2f;
+            y *= 2f;
+            z *= 2f;
+            w *= 2f;
+            u *= 2f;
+            v *= 2f;
+
+            final int idx = (int) (latest * 8192) & TrigTools.TABLE_MASK;
+            float a = TrigTools.SIN_TABLE[idx];
+            float b = TrigTools.SIN_TABLE[idx + (8192 / 6) & TrigTools.TABLE_MASK];
+            float c = TrigTools.SIN_TABLE[idx + (8192 * 2 / 6) & TrigTools.TABLE_MASK];
+            float d = TrigTools.SIN_TABLE[idx + (8192 * 3 / 6) & TrigTools.TABLE_MASK];
+            float e = TrigTools.SIN_TABLE[idx + (8192 * 4 / 6) & TrigTools.TABLE_MASK];
+            float f = TrigTools.SIN_TABLE[idx + (8192 * 5 / 6) & TrigTools.TABLE_MASK];
+
+            amp *= 0.5f;
+            sum += (latest = wrapped.getNoiseWithSeed(x + a, y + b, z + c, w + d, u + e, v + f, seed + i)) * amp;
         }
 
         return sum * ((1 << octaves) - 1);
