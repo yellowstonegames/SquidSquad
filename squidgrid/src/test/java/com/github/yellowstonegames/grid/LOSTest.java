031767896f62ba9696199b1231d401a15fcf6020
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-01-22 11:20:53 -0800

    SadConsole's Bresenham seems nice and clean.
    Using this approach would be more lambda-heavy, but it should have the same result.

--- squidgrid/src/test/java/com/github/yellowstonegames/grid/LOSTest.java
+++ squidgrid/src/test/java/com/github/yellowstonegames/grid/LOSTest.java
@@ -1,7 +1,11 @@
 package com.github.yellowstonegames.grid;
 
+import com.github.tommyettinger.digital.TrigTools;
 import com.github.tommyettinger.ds.ObjectList;
 import com.github.tommyettinger.digital.ArrayTools;
+import com.github.tommyettinger.function.IntIntPredicate;
+import com.github.tommyettinger.random.EnhancedRandom;
+import com.github.tommyettinger.random.WhiskerRandom;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -42,13 +46,13 @@ public class LOSTest {
         ArrayTools.fill(res, 1f);
         ArrayTools.fill(floors, 0f);
         ArrayTools.insert(floors, res, 1, 1);
-//        ObjectList<Coord> buffer = new ObjectList<>(40);
+        ObjectList<Coord> buffer = new ObjectList<>(40);
         for (int length = 0; length < 10; length++) {
             System.out.println("\n" + length);
             ArrayTools.insert(interior, grid, 1, 1);
             for (int x = 1; x < 20; x++) {
                 for (int y = 1; y < 20; y++) {
-                    grid[x][y] = (BresenhamLine.reachableEuclidean(10, 10, x, y, length, res, null)) ? '*' : '0';
+                    grid[x][y] = (BresenhamLine.reachableEuclidean(10, 10, x, y, length, res, buffer)) ? '*' : '0';
                 }
             }
             for (int y = 0; y < 21; y++) {
@@ -106,5 +110,135 @@ public class LOSTest {
             }
         }
     }
+    public static boolean sadLine(int x1, int y1, int x2, int y2, IntIntPredicate plot)
+    {
+        int w = x2 - x1;
+        int h = y2 - y1;
+        int dx1 = 0, dy1 = 0, dx2 = 0, dy2 = 0;
+        if (w < 0) dx1 = -1; else if (w > 0) dx1 = 1;
+        if (h < 0) dy1 = -1; else if (h > 0) dy1 = 1;
+        if (w < 0) dx2 = -1; else if (w > 0) dx2 = 1;
+        int longest = Math.abs(w);
+        int shortest = Math.abs(h);
+        if (!(longest > shortest))
+        {
+            longest = Math.abs(h);
+            shortest = Math.abs(w);
+            if (h < 0) dy2 = -1; else if (h > 0) dy2 = 1;
+            dx2 = 0;
+        }
+        int numerator = longest >> 1;
+        for (int i = 0; i <= longest; i++)
+        {
+            if(!plot.test(x1, y1)) return false;
+            numerator += shortest;
+            if (!(numerator < longest))
+            {
+                numerator -= longest;
+                x1 += dx1;
+                y1 += dy1;
+            }
+            else
+            {
+                x1 += dx2;
+                y1 += dy2;
+            }
+        }
+        return true;
+    }
+    @Test
+    public void testSadLine() {
+        char[][] grid = new char[21][21], interior = new char[19][19];
+        ArrayTools.fill(grid, '#');
+        ArrayTools.fill(interior, '.');
+        ArrayTools.insert(interior, grid, 1, 1);
+        float[][] res = new float[21][21], floors = new float[19][19];
+        ArrayTools.fill(res, 1f);
+        ArrayTools.insert(floors, res, 1, 1);
+        final ObjectList<Coord> buffer = new ObjectList<>(40);
+        for (int length = 0; length < 10; length++) {
+            System.out.println("\n" + length);
+            ArrayTools.insert(interior, grid, 1, 1);
+            for (int x = 1; x < 20; x++) {
+                for (int y = 1; y < 20; y++) {
+                    buffer.clear();
+                    int finalLength = length;
+                    grid[x][y] = (sadLine(10, 10, x, y, (a, b) -> res[a][b] < 1f && buffer.add(Coord.get(a, b)) && buffer.size() <= finalLength
+                    )) ? '*' : '0';
+                }
+            }
+            for (int y = 0; y < 21; y++) {
+                for (int x = 0; x < 21; x++) {
+                    System.out.print(grid[x][y]);
+                }
+                System.out.println();
+            }
+        }
+    }
+    @Test
+    public void testBresenhamLineDraw() {
+        char[][] grid = new char[21][21], interior = new char[19][19];
+        ArrayTools.fill(grid, '#');
+        ArrayTools.fill(interior, '.');
+        ArrayTools.insert(interior, grid, 1, 1);
+        float[][] res = new float[21][21], floors = new float[19][19];
+        ArrayTools.fill(res, 1f);
+        ArrayTools.insert(floors, res, 1, 1);
+        final ObjectList<Coord> buffer = new ObjectList<>(40);
+        final EnhancedRandom random = new WhiskerRandom(123);
+        for (int length = 0; length < 10; length++) {
+            System.out.println("\n" + length);
+            ArrayTools.insert(interior, grid, 1, 1);
+            {
+                buffer.clear();
+                int finalLength = length;
+                float theta = random.nextFloat();
+                int x = Math.round(TrigTools.cosTurns(theta) * length + 10);
+                int y = Math.round(TrigTools.sinTurns(theta) * length + 10);
 
+                BresenhamLine.reachable(10, 10, x, y, length, res, buffer);
+                for(Coord c : buffer)
+                    grid[c.x][c.y] = '*';
+            }
+            for (int y = 0; y < 21; y++) {
+                for (int x = 0; x < 21; x++) {
+                    System.out.print(grid[x][y]);
+                }
+                System.out.println();
+            }
+        }
+    }
+    @Test
+    public void testSadLineDraw() {
+        char[][] grid = new char[21][21], interior = new char[19][19];
+        ArrayTools.fill(grid, '#');
+        ArrayTools.fill(interior, '.');
+        ArrayTools.insert(interior, grid, 1, 1);
+        float[][] res = new float[21][21], floors = new float[19][19];
+        ArrayTools.fill(res, 1f);
+        ArrayTools.insert(floors, res, 1, 1);
+        final ObjectList<Coord> buffer = new ObjectList<>(40);
+        final EnhancedRandom random = new WhiskerRandom(123);
+        for (int length = 0; length < 10; length++) {
+            System.out.println("\n" + length);
+            ArrayTools.insert(interior, grid, 1, 1);
+            {
+                buffer.clear();
+                int finalLength = length;
+                float theta = random.nextFloat();
+                int x = Math.round(TrigTools.cosTurns(theta) * length + 10);
+                int y = Math.round(TrigTools.sinTurns(theta) * length + 10);
+
+                sadLine(10, 10, x, y, (a, b) -> res[a][b] < 1f && buffer.add(Coord.get(a, b)) && buffer.size() < finalLength);
+                for(Coord c : buffer)
+                    grid[c.x][c.y] = '*';
+            }
+            for (int y = 0; y < 21; y++) {
+                for (int x = 0; x < 21; x++) {
+                    System.out.print(grid[x][y]);
+                }
+                System.out.println();
+            }
+        }
+    }
 }
