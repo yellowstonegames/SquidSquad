dee240a83fac52c013c8c40c42c572e9537a06d4
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-03-13 22:32:28 -0700

    Thanks, shapedrawer and earlygrey!
    I wasn't able to get ImmediateModeRenderer20 to appear on screenshots using FrameBuffer, but it worked just about immediately with shapedrawer. This is my first time using the library, and I'm happy with it! The output looks like https://i.imgur.com/0oZX5fR.png

--- squidworld/src/test/java/com/github/yellowstonegames/world/InterpolationsGraphing.java
+++ squidworld/src/test/java/com/github/yellowstonegames/world/InterpolationsGraphing.java
@@ -22,27 +22,28 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.PixmapIO;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ImmediateModeRenderer20;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.github.tommyettinger.ds.ObjectList;
 import com.github.tommyettinger.textra.Font;
 import com.github.tommyettinger.textra.KnownFonts;
 import com.github.tommyettinger.textra.Layout;
 import com.github.yellowstonegames.core.DescriptiveColor;
 import com.github.yellowstonegames.core.Interpolations;
 import com.github.yellowstonegames.core.Interpolations.Interpolator;
-
-import static com.badlogic.gdx.graphics.GL20.GL_LINES;
+import space.earlygrey.shapedrawer.ShapeDrawer;
 
 public class InterpolationsGraphing extends ApplicationAdapter {
+    static final boolean WRITE = true;
+
     private static final int width = 300, height = 420;
 
     Interpolator current;
     Font font;
-    ImmediateModeRenderer20 renderer;
+    ShapeDrawer sd;
     SpriteBatch batch;
     ScreenViewport view;
     Layout name;
@@ -53,11 +54,21 @@ public class InterpolationsGraphing extends ApplicationAdapter {
         font = KnownFonts.getCozette();
         view = new ScreenViewport();
         batch = new SpriteBatch();
-        renderer = new ImmediateModeRenderer20(width * 3, false, true, 0);
+        sd = new ShapeDrawer(batch, font.mapping.get(font.solidBlock));
         interpolators = Interpolations.getInterpolatorArray();
         index = 0;
         current = interpolators[index];
         name = font.markup("[BLACK]"+ current.tag, name = new Layout(font));
+        if(WRITE){
+            for (int i = 0; i < interpolators.length; i++) {
+                current = interpolators[i];
+                render();
+                Pixmap pixmap = Pixmap.createFromFrameBuffer(0, 0, width, height);
+                PixmapIO.writePNG(Gdx.files.local("out/interpolators/" + current.tag + ".png"), pixmap, 9, true);
+                pixmap.dispose();
+            }
+            Gdx.app.exit();
+        }
     }
 
     @Override
@@ -71,43 +82,24 @@ public class InterpolationsGraphing extends ApplicationAdapter {
             name = font.markup("[BLACK]"+ current.tag, name.clear());
         }
         ScreenUtils.clear(Color.WHITE);
-        renderer.begin(view.getCamera().combined, GL_LINES);
+        batch.begin();
 
         // horizontal graph lines
         for (int i = 20; i < height; i+=40) {
-            renderer.color(Color.CYAN);
-            renderer.vertex(0, i, 0);
-            renderer.color(Color.CYAN);
-            renderer.vertex(width, i, 0);
+            sd.line(0, i, width, i, Color.CYAN);
         }
         // vertical graph lines
         for (int i = 20; i < width; i+=40) {
-            renderer.color(Color.CYAN);
-            renderer.vertex(i, 0, 0);
-            renderer.color(Color.CYAN);
-            renderer.vertex(i, height, 0);
+            sd.line(i, 0, i, height, Color.CYAN);
         }
         // x-axis
-        renderer.color(Color.NAVY);
-        renderer.vertex(0, 100, 0);
-        renderer.color(Color.NAVY);
-        renderer.vertex(width, 100, 0);
+        sd.line(0, 100, width, 100, Color.NAVY);
         // y-axis
-        renderer.color(Color.NAVY);
-        renderer.vertex(60, 0, 0);
-        renderer.color(Color.NAVY);
-        renderer.vertex(60, height, 0);
-        renderer.end();
+        sd.line(60, 0, 60, height, Color.NAVY);
         // line where y == 1
-        renderer.color(Color.LIGHT_GRAY);
-        renderer.vertex(0, 300, 0);
-        renderer.color(Color.LIGHT_GRAY);
-        renderer.vertex(width, 300, 0);
+        sd.line(0, 300, width, 300, Color.LIGHT_GRAY);
         // line where x == 1
-        renderer.color(Color.LIGHT_GRAY);
-        renderer.vertex(260, 0, 0);
-        renderer.color(Color.LIGHT_GRAY);
-        renderer.vertex(260, height, 0);
+        sd.line(260, 0, 260, height, Color.LIGHT_GRAY);
 
         float h0, h1 = 101;
         for (int i = 0; i <= 200; i++) {
@@ -115,14 +107,61 @@ public class InterpolationsGraphing extends ApplicationAdapter {
             h0 = h1;
             h1 = current.apply(101, 301, f);
             float gradient = DescriptiveColor.oklabIntToFloat(DescriptiveColor.oklabByHSL(0.75f + 0.25f * f, 1f, 0.5f, 1f));
-            renderer.color(gradient);
-            renderer.vertex(59 + i, h0, 0);
-            renderer.color(gradient);
-            renderer.vertex(60 + i, h1, 0);
+            sd.setColor(gradient);
+            sd.line(59 + i, h0, 60 + i, h1);
         }
-        renderer.end();
 
-        batch.begin();
+//        renderer.begin(view.getCamera().combined, GL_LINES);
+//
+//        // horizontal graph lines
+//        for (int i = 20; i < height; i+=40) {
+//            renderer.color(Color.CYAN);
+//            renderer.vertex(0, i, 0);
+//            renderer.color(Color.CYAN);
+//            renderer.vertex(width, i, 0);
+//        }
+//        // vertical graph lines
+//        for (int i = 20; i < width; i+=40) {
+//            renderer.color(Color.CYAN);
+//            renderer.vertex(i, 0, 0);
+//            renderer.color(Color.CYAN);
+//            renderer.vertex(i, height, 0);
+//        }
+//        // x-axis
+//        renderer.color(Color.NAVY);
+//        renderer.vertex(0, 100, 0);
+//        renderer.color(Color.NAVY);
+//        renderer.vertex(width, 100, 0);
+//        // y-axis
+//        renderer.color(Color.NAVY);
+//        renderer.vertex(60, 0, 0);
+//        renderer.color(Color.NAVY);
+//        renderer.vertex(60, height, 0);
+//        renderer.end();
+//        // line where y == 1
+//        renderer.color(Color.LIGHT_GRAY);
+//        renderer.vertex(0, 300, 0);
+//        renderer.color(Color.LIGHT_GRAY);
+//        renderer.vertex(width, 300, 0);
+//        // line where x == 1
+//        renderer.color(Color.LIGHT_GRAY);
+//        renderer.vertex(260, 0, 0);
+//        renderer.color(Color.LIGHT_GRAY);
+//        renderer.vertex(260, height, 0);
+//
+//        float h0, h1 = 101;
+//        for (int i = 0; i <= 200; i++) {
+//            float f = i / 200f;
+//            h0 = h1;
+//            h1 = current.apply(101, 301, f);
+//            float gradient = DescriptiveColor.oklabIntToFloat(DescriptiveColor.oklabByHSL(0.75f + 0.25f * f, 1f, 0.5f, 1f));
+//            renderer.color(gradient);
+//            renderer.vertex(59 + i, h0, 0);
+//            renderer.color(gradient);
+//            renderer.vertex(60 + i, h1, 0);
+//        }
+//        renderer.end();
+
         float level = -0.4f;
         for (int i = 15; i <= 400; i+=40) {
             font.drawMarkupText(batch, String.format("[darker gray]%4.1f", level), 32, i);
