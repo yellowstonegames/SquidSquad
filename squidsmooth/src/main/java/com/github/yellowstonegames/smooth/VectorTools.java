dd9fae5ff31d8b9e2e0d4755c16ac385cd5dc883
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-03-24 15:26:13 -0700

    Add some Vector2 ser/deser code.

--- squidsmooth/src/main/java/com/github/yellowstonegames/smooth/VectorTools.java
+++ squidsmooth/src/main/java/com/github/yellowstonegames/smooth/VectorTools.java
@@ -17,8 +17,10 @@
 package com.github.yellowstonegames.smooth;
 
 import com.badlogic.gdx.math.Vector2;
+import com.github.tommyettinger.digital.Base;
 import com.github.tommyettinger.digital.TrigTools;
 import com.github.tommyettinger.random.EnhancedRandom;
+import com.github.yellowstonegames.core.StringTools;
 
 /**
  * Utilities for handling floating-point positions, to be added to as needed.
@@ -33,5 +35,49 @@ public class VectorTools {
     public static Vector2 randomUnit(Vector2 receiving, EnhancedRandom random) {
         final int index = random.next(14);
         return receiving.set(TrigTools.SIN_TABLE[index], TrigTools.SIN_TABLE[index + 4096 & TrigTools.TABLE_MASK]);
+    }
+    private static final StringBuilder sb = new StringBuilder(50);
+
+    /**
+     * Appends the serialized representation of the given Vector2 to the given StringBuilder.
+     * @param sb a non-null StringBuilder
+     * @param vec a non-null Vector2 to serialize
+     * @return sb, after modifications
+     */
+    public static StringBuilder appendSerializedString(StringBuilder sb, Vector2 vec) {
+        return Base.BASE10.appendFriendly(Base.BASE10.appendFriendly(sb, vec.x).append('~'), vec.y);
+    }
+
+    /**
+     * Serializes the given Vector2 and returns it as a new String.
+     * @param vec a non-null Vector2 to serialize
+     * @return the serialized String version of vec
+     */
+    public static String serializeString(Vector2 vec) {
+        sb.setLength(0);
+        return appendSerializedString(sb, vec).toString();
+    }
+
+    /**
+     * Deserializes the given String to get the values this assigns to the Vector2 {@code receiving}.
+     * @param data a String containing a serialized Vector2
+     * @param receiving a non-null Vector2 that will have its contents reassigned
+     * @return receiving, after modifications
+     */
+    public static Vector2 deserializeString(String data, Vector2 receiving) {
+        int idx = data.indexOf('~');
+        receiving.x = Base.BASE10.readFloat(data, 0, idx);
+        receiving.y = Base.BASE10.readFloat(data, idx+1, data.length());
+        return receiving;
+    }
+
+    /**
+     * Deserializes a Vector2 from the given String using {@link #deserializeString(String, Vector2)}, allocating a new
+     * Vector2 that this returns.
+     * @param data a String containing a serialized Vector2
+     * @return a new Vector2 using the values from data
+     */
+    public static Vector2 deserializeString(String data) {
+        return deserializeString(data, new Vector2());
     }
 }
