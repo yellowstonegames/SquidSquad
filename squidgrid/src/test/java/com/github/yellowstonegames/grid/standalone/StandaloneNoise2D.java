4d567293d15f574befbd713d0335678a7fbaaccb
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-06-02 15:41:44 -0700

    Make a standalone Poisson Disk class. Uses GDX.

--- squidgrid/src/test/java/com/github/yellowstonegames/grid/standalone/StandaloneNoise2D.java
+++ squidgrid/src/test/java/com/github/yellowstonegames/grid/standalone/StandaloneNoise2D.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.github.yellowstonegames.grid;
+package com.github.yellowstonegames.grid.standalone;
 
 /**
  * Simplex noise function in 2D only. This is a smooth type of noise that works well if you can request multiple octaves
