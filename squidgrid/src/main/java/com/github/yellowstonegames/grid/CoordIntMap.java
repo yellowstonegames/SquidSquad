a76f2b86e01078a15b7db438e140e4a23542b7e2
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-06-16 22:08:20 -0700

    And now CoordInt (Ordered) Map.

--- squidgrid/src/main/java/com/github/yellowstonegames/grid/CoordIntMap.java
+++ squidgrid/src/main/java/com/github/yellowstonegames/grid/CoordIntMap.java
@@ -16,55 +16,45 @@
 
 package com.github.yellowstonegames.grid;
 
-import com.github.tommyettinger.ds.ObjectLongMap;
-import com.github.tommyettinger.ds.ObjectLongOrderedMap;
+import com.github.tommyettinger.ds.ObjectIntMap;
 import com.github.tommyettinger.ds.PrimitiveCollection;
-import com.github.tommyettinger.ds.support.sort.LongComparator;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 import java.util.Collection;
 
 /**
- * A variant on jdkgdxds' {@link ObjectLongOrderedMap} class that only uses Coord keys, and can do so more efficiently.
+ * A variant on jdkgdxds' {@link ObjectIntMap} class that only uses Coord keys, and can do so more efficiently.
  * This assumes all Coord keys are in the Coord pool; that is, {@link Coord#expandPoolTo(int, int)} has been called with
  * the maximum values for Coord x and y. If you cannot be sure that the Coord pool will hold keys placed into here, you
- * should use a normal {@link ObjectLongOrderedMap} instead, since some optimizations here require Coord keys to be in
- * the pool. This does much better if all Coord keys use non-negative x and y values.
+ * should use a normal {@link ObjectIntMap} instead, since some optimizations here require Coord keys to be in the
+ * pool. This does much better if all Coord keys use non-negative x and y values.
  * <br>
- * You can create a CoordLongOrderedMap with {@link #fromArray2D(long[][], long, long)} if you have a 2D long array and
+ * You can create a CoordIntMap with {@link #fromArray2D(int[][], int, int)} if you have a 2D int array and
  * want to get the positions within some range.
  */
-public class CoordLongOrderedMap extends ObjectLongOrderedMap<Coord> {
-    public CoordLongOrderedMap() {
+public class CoordIntMap extends ObjectIntMap<Coord> {
+    public CoordIntMap() {
         super();
     }
 
-    public CoordLongOrderedMap(int initialCapacity) {
+    public CoordIntMap(int initialCapacity) {
         super(initialCapacity);
     }
 
-    public CoordLongOrderedMap(int initialCapacity, float loadFactor) {
+    public CoordIntMap(int initialCapacity, float loadFactor) {
         super(initialCapacity, loadFactor);
     }
 
-    public CoordLongOrderedMap(ObjectLongOrderedMap<? extends Coord> map) {
+    public CoordIntMap(ObjectIntMap<? extends Coord> map) {
         super(map);
     }
 
-    public CoordLongOrderedMap(ObjectLongMap<? extends Coord> map) {
-        super(map);
-    }
-
-    public CoordLongOrderedMap(Coord[] keys, long[] values) {
+    public CoordIntMap(Coord[] keys, int[] values) {
         super(keys, values);
     }
 
-    public CoordLongOrderedMap(Collection<? extends Coord> keys, PrimitiveCollection.OfLong values) {
+    public CoordIntMap(Collection<? extends Coord> keys, PrimitiveCollection.OfInt values) {
         super(keys, values);
-    }
-
-    public CoordLongOrderedMap(ObjectLongOrderedMap<? extends Coord> other, int offset, int count) {
-        super(other, offset, count);
     }
 
     @Override
@@ -81,15 +71,15 @@ public class CoordLongOrderedMap extends ObjectLongOrderedMap<Coord> {
      * Constructs a single-entry map given one key and one value.
      * This is mostly useful as an optimization for {@link #with(Coord, Number, Object...)}
      * when there's no "rest" of the keys or values. Like the more-argument with(), this will
-     * convert its Number value to a primitive long, regardless of which Number type was used.
+     * convert its Number value to a primitive int, regardless of which Number type was used.
      *
      * @param key0   the first and only key; always a Coord
-     * @param value0 the first and only value; will be converted to primitive long
+     * @param value0 the first and only value; will be converted to primitive int
      * @return a new map containing just the entry mapping key0 to value0
      */
-    public static CoordLongOrderedMap with (Coord key0, Number value0) {
-        CoordLongOrderedMap map = new CoordLongOrderedMap(1);
-        map.put(key0, value0.longValue());
+    public static CoordIntMap with (Coord key0, Number value0) {
+        CoordIntMap map = new CoordIntMap(1);
+        map.put(key0, value0.intValue());
         return map;
     }
 
@@ -97,22 +87,22 @@ public class CoordLongOrderedMap extends ObjectLongOrderedMap<Coord> {
      * Constructs a map given alternating keys and values.
      * This can be useful in some code-generation scenarios, or when you want to make a
      * map conveniently by-hand and have it populated at the start. You can also use
-     * {@link #CoordLongOrderedMap(Coord[], long[])}, which takes all keys and then all values.
+     * {@link #CoordIntMap(Coord[], int[])}, which takes all keys and then all values.
      * All values must be some type of boxed Number, such as {@link Integer}
-     * or {@link Double}, and will be converted to primitive {@code long}s. Any keys that don't
+     * or {@link Double}, and will be converted to primitive {@code int}s. Any keys that don't
      * have Coord as their type or values that aren't {@code Number}s have that entry skipped.
      *
      * @param key0   the first key; always a Coord
-     * @param value0 the first value; will be converted to primitive long
+     * @param value0 the first value; will be converted to primitive int
      * @param rest   an array or varargs of alternating Coord, Number, Coord, Number... elements
      * @return a new map containing the given keys and values
      */
-    public static CoordLongOrderedMap with (Coord key0, Number value0, Object... rest) {
-        CoordLongOrderedMap map = new CoordLongOrderedMap(1 + (rest.length >>> 1));
-        map.put(key0, value0.longValue());
+    public static CoordIntMap with (Coord key0, Number value0, Object... rest) {
+        CoordIntMap map = new CoordIntMap(1 + (rest.length >>> 1));
+        map.put(key0, value0.intValue());
         for (int i = 1; i < rest.length; i += 2) {
             try {
-                map.put((Coord) rest[i - 1], ((Number)rest[i]).longValue());
+                map.put((Coord) rest[i - 1], ((Number)rest[i]).intValue());
             } catch (ClassCastException ignored) {
             }
         }
@@ -120,21 +110,20 @@ public class CoordLongOrderedMap extends ObjectLongOrderedMap<Coord> {
     }
 
     /**
-     * Given a 2D long array (which should usually be rectangular), this finds the positions with values between min
-     * (inclusive) and max (also inclusive), and places them in a newly-allocated CoordLongOrderedMap. You may want to
-     * sort this by value, using {@link #sortByValue(LongComparator)}, so the highest or lowest long values belong to
-     * the first or last items in the order.
-     * @param array a usually-rectangular 2D long array
+     * Given a 2D int array (which should usually be rectangular), this finds the positions with values between min
+     * (inclusive) and max (also inclusive), and places them in a newly-allocated CoordIntMap. If you need to
+     * get a sorted ordering, use {@link CoordIntOrderedMap#fromArray2D(int[][], int, int)} and sort the result.
+     * @param array a usually-rectangular 2D int array
      * @param min the inclusive minimum value
      * @param max the inclusive maximum value
-     * @return a new CoordLongOrderedMap containing all positions with in-range values, mapped to those values.
+     * @return a new CoordIntMap containing all positions with in-range values, mapped to those values.
      */
-    public static CoordLongOrderedMap fromArray2D(long[][] array, long min, long max) {
+    public static CoordIntMap fromArray2D(int[][] array, int min, int max) {
         final int width = array.length;
-        CoordLongOrderedMap map = new CoordLongOrderedMap(width * array[0].length);
+        CoordIntMap map = new CoordIntMap(width * array[0].length);
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < array[x].length; y++) {
-                final long n = array[x][y];
+                final int n = array[x][y];
                 if (n >= min && n <= max) {
                     map.put(Coord.get(x, y), n);
                 }
@@ -142,4 +131,5 @@ public class CoordLongOrderedMap extends ObjectLongOrderedMap<Coord> {
         }
         return map;
     }
+
 }
