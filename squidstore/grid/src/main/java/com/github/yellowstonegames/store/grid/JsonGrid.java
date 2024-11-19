0d7c650f402fee10a337d38ee9ad7e33f624327c
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-11-19 00:15:41 -0800

    Point4Int JSON ser/deser.

--- squidstore/grid/src/main/java/com/github/yellowstonegames/store/grid/JsonGrid.java
+++ squidstore/grid/src/main/java/com/github/yellowstonegames/store/grid/JsonGrid.java
@@ -45,6 +45,7 @@ public final class JsonGrid {
         registerCoord(json);
         registerPoint2Int(json);
         registerPoint3Int(json);
+        registerPoint4Int(json);
         registerRegion(json);
         registerCoordSet(json);
         registerCoordOrderedSet(json);
@@ -159,6 +160,33 @@ public final class JsonGrid {
             public Point3Int read(Json json, JsonValue jsonData, Class type) {
                 if (jsonData == null || jsonData.isNull()) return null;
                 return new Point3Int(jsonData.getInt("x", 0), jsonData.getInt("y", 0), jsonData.getInt("z", 0));
+            }
+        });
+    }
+
+    /**
+     * Registers Point4Int with the given Json object, so Point4Int can be written to and read from JSON.
+     *
+     * @param json a libGDX Json object that will have a serializer registered
+     */
+    public static void registerPoint4Int(@NonNull Json json) {
+        json.addClassTag("Pt4I", Point4Int.class);
+        json.setSerializer(Point4Int.class, new Json.Serializer<Point4Int>() {
+            @Override
+            public void write(Json json, Point4Int object, Class knownType) {
+                json.writeObjectStart(Point4Int.class, knownType);
+                json.writeValue("x", object.x);
+                json.writeValue("y", object.y);
+                json.writeValue("z", object.z);
+                json.writeValue("w", object.w);
+                json.writeObjectEnd();
+            }
+
+            @Override
+            public Point4Int read(Json json, JsonValue jsonData, Class type) {
+                if (jsonData == null || jsonData.isNull()) return null;
+                return new Point4Int(jsonData.getInt("x", 0), jsonData.getInt("y", 0),
+                        jsonData.getInt("z", 0), jsonData.getInt("w", 0));
             }
         });
     }
