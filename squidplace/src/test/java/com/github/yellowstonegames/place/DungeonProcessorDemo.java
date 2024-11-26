34f086d046c697f7df49fd6daa24f694ba791deb
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-11-25 23:26:12 -0800

    JitPack docs pretty much say "Guess," so...
    I have no idea what YAML config JitPack takes, since it isn't documented clearly and the examples they point to, to show you how to use jitpack.yml files, don't use jitpack.yml files.

--- squidplace/src/test/java/com/github/yellowstonegames/place/DungeonProcessorDemo.java
+++ squidplace/src/test/java/com/github/yellowstonegames/place/DungeonProcessorDemo.java
@@ -17,13 +17,15 @@
 package com.github.yellowstonegames.place;
 
 import com.github.tommyettinger.random.PouchRandom;
+import com.github.yellowstonegames.grid.LineTools;
 import com.github.yellowstonegames.place.tileset.TilesetType;
 
 public class DungeonProcessorDemo {
 
+    public static final long SEED = 0xBEEFBEEFBEEFBEEFL;
     public static void main(String[] args) {
-        PouchRandom random = new PouchRandom(0xB0BAFE77BA77L);
-        DungeonProcessor gen = new DungeonProcessor(75, 125, random);
+        PouchRandom random = new PouchRandom(SEED);
+        DungeonProcessor gen = new DungeonProcessor(50, 35, random);
 
 //        gen.addGrass(0, 10);
 //        gen.addWater(0, 6);
@@ -35,12 +37,12 @@ public class DungeonProcessorDemo {
 //        DungeonBoneGen bone = new DungeonBoneGen(random.copy());
 
         for(TilesetType tt : TilesetType.values()){
-            random.setSeed(0xB0BAFE77BA77L);
+            random.setSeed(SEED);
             System.out.println(tt.name());
-//            DungeonTools.debugPrint(LineTools.hashesToLines(gen.generate(tt), true));
+            DungeonTools.debugPrint(LineTools.hashesToLines(gen.generate(tt), true));
 //            bone.generate(tt, 60, 32);
 //            DungeonTools.debugPrint(bone.region.not().thin().not().removeEdges().toChars());
-            DungeonTools.debugPrint(gen.generate(tt));
+            DungeonTools.debugPrint(gen.dungeon);
         }
     }
 }
