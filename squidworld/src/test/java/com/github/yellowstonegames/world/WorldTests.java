762e9e7c96e0635306eaee1d788c36f35f0286a7
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-12-30 21:17:35 -0800

    That should be every rectangular map...
    ... Except MimicWorldMap's default.

--- squidworld/src/test/java/com/github/yellowstonegames/world/WorldTests.java
+++ squidworld/src/test/java/com/github/yellowstonegames/world/WorldTests.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 public class WorldTests {
     public static final int SIZE = 100;
+
     @Test
     public void testElliptical() {
         for (int i = 0; i < 10; i++) {
@@ -16,6 +17,86 @@ public class WorldTests {
             String ser = world.stringSerialize();
             System.out.println("Serialized data is " + ser.length() + " chars long.");
             wmv.setWorld(EllipticalWorldMap.recreateFromString(ser));
+            wmv.generate();
+            int[][] cm1 = wmv.show();
+            Assert.assertEquals(Hasher.intArray2DHashBulk64.hash64(i, cm0), Hasher.intArray2DHashBulk64.hash64(i, cm1));
+        }
+    }
+
+    @Test
+    public void testHexagonal() {
+        for (int i = 0; i < 10; i++) {
+            HexagonalWorldMap world = new HexagonalWorldMap(i, SIZE * 2, SIZE);
+            BlendedWorldMapView wmv = new BlendedWorldMapView(world);
+            wmv.generate();
+            int[][] cm0 = wmv.show();
+            String ser = world.stringSerialize();
+            System.out.println("Serialized data is " + ser.length() + " chars long.");
+            wmv.setWorld(HexagonalWorldMap.recreateFromString(ser));
+            wmv.generate();
+            int[][] cm1 = wmv.show();
+            Assert.assertEquals(Hasher.intArray2DHashBulk64.hash64(i, cm0), Hasher.intArray2DHashBulk64.hash64(i, cm1));
+        }
+    }
+
+    @Test
+    public void testHyperelliptical() {
+        for (int i = 0; i < 10; i++) {
+            HyperellipticalWorldMap world = new HyperellipticalWorldMap(i, SIZE * 2, SIZE);
+            BlendedWorldMapView wmv = new BlendedWorldMapView(world);
+            wmv.generate();
+            int[][] cm0 = wmv.show();
+            String ser = world.stringSerialize();
+            System.out.println("Serialized data is " + ser.length() + " chars long.");
+            wmv.setWorld(HyperellipticalWorldMap.recreateFromString(ser));
+            wmv.generate();
+            int[][] cm1 = wmv.show();
+            Assert.assertEquals(Hasher.intArray2DHashBulk64.hash64(i, cm0), Hasher.intArray2DHashBulk64.hash64(i, cm1));
+        }
+    }
+
+    @Test
+    public void testLatLon() {
+        for (int i = 0; i < 10; i++) {
+            LatLonWorldMap world = new LatLonWorldMap(i, SIZE * 2, SIZE);
+            BlendedWorldMapView wmv = new BlendedWorldMapView(world);
+            wmv.generate();
+            int[][] cm0 = wmv.show();
+            String ser = world.stringSerialize();
+            System.out.println("Serialized data is " + ser.length() + " chars long.");
+            wmv.setWorld(LatLonWorldMap.recreateFromString(ser));
+            wmv.generate();
+            int[][] cm1 = wmv.show();
+            Assert.assertEquals(Hasher.intArray2DHashBulk64.hash64(i, cm0), Hasher.intArray2DHashBulk64.hash64(i, cm1));
+        }
+    }
+
+    @Test
+    public void testRoundSide() {
+        for (int i = 0; i < 10; i++) {
+            RoundSideWorldMap world = new RoundSideWorldMap(i, SIZE * 2, SIZE);
+            BlendedWorldMapView wmv = new BlendedWorldMapView(world);
+            wmv.generate();
+            int[][] cm0 = wmv.show();
+            String ser = world.stringSerialize();
+            System.out.println("Serialized data is " + ser.length() + " chars long.");
+            wmv.setWorld(RoundSideWorldMap.recreateFromString(ser));
+            wmv.generate();
+            int[][] cm1 = wmv.show();
+            Assert.assertEquals(Hasher.intArray2DHashBulk64.hash64(i, cm0), Hasher.intArray2DHashBulk64.hash64(i, cm1));
+        }
+    }
+
+    @Test
+    public void testStretch() {
+        for (int i = 0; i < 10; i++) {
+            StretchWorldMap world = new StretchWorldMap(i, SIZE * 2, SIZE);
+            BlendedWorldMapView wmv = new BlendedWorldMapView(world);
+            wmv.generate();
+            int[][] cm0 = wmv.show();
+            String ser = world.stringSerialize();
+            System.out.println("Serialized data is " + ser.length() + " chars long.");
+            wmv.setWorld(StretchWorldMap.recreateFromString(ser));
             wmv.generate();
             int[][] cm1 = wmv.show();
             Assert.assertEquals(Hasher.intArray2DHashBulk64.hash64(i, cm0), Hasher.intArray2DHashBulk64.hash64(i, cm1));
