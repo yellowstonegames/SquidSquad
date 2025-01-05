bf2d24383004e2173ad6f98d41b25c16327427b6
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-01-04 21:32:51 -0800

    Woo, there are the tests!

--- squidwrath/world/src/test/java/com/github/yellowstonegames/wrath/world/WorldTest.java
+++ squidwrath/world/src/test/java/com/github/yellowstonegames/wrath/world/WorldTest.java
@@ -16,7 +16,7 @@
 
 package com.github.yellowstonegames.wrath.world;
 
-import com.github.yellowstonegames.world.EllipticalWorldMap;
+import com.github.yellowstonegames.world.*;
 import org.apache.fury.Fury;
 import org.apache.fury.logging.LoggerFactory;
 import org.junit.Assert;
@@ -34,6 +34,171 @@ public class WorldTest {
         {
             byte[] bytes = fury.serializeJavaObject(data);
             EllipticalWorldMap data2 = fury.deserializeJavaObject(bytes, EllipticalWorldMap.class);
+            Assert.assertEquals(data, data2);
+        }
+    }
+
+    @Test
+    public void testGlobeMap() {
+        LoggerFactory.disableLogging();
+        Fury fury = Fury.builder().withLanguage(org.apache.fury.config.Language.JAVA).build();
+        fury.registerSerializer(GlobeMap.class, new GlobeMapSerializer(fury));
+
+        GlobeMap data = new GlobeMap(123, 100, 100);
+        data.generate();
+        {
+            byte[] bytes = fury.serializeJavaObject(data);
+            GlobeMap data2 = fury.deserializeJavaObject(bytes, GlobeMap.class);
+            Assert.assertEquals(data, data2);
+        }
+    }
+
+    @Test
+    public void testHexagonalWorldMap() {
+        LoggerFactory.disableLogging();
+        Fury fury = Fury.builder().withLanguage(org.apache.fury.config.Language.JAVA).build();
+        fury.registerSerializer(HexagonalWorldMap.class, new HexagonalWorldMapSerializer(fury));
+
+        HexagonalWorldMap data = new HexagonalWorldMap(123, 200, 100);
+        data.generate();
+        {
+            byte[] bytes = fury.serializeJavaObject(data);
+            HexagonalWorldMap data2 = fury.deserializeJavaObject(bytes, HexagonalWorldMap.class);
+            Assert.assertEquals(data, data2);
+        }
+    }
+    
+    @Test
+    public void testHyperellipticalWorldMap() {
+        LoggerFactory.disableLogging();
+        Fury fury = Fury.builder().withLanguage(org.apache.fury.config.Language.JAVA).build();
+        fury.registerSerializer(HyperellipticalWorldMap.class, new HyperellipticalWorldMapSerializer(fury));
+
+        HyperellipticalWorldMap data = new HyperellipticalWorldMap(123, 200, 100);
+        data.generate();
+        {
+            byte[] bytes = fury.serializeJavaObject(data);
+            HyperellipticalWorldMap data2 = fury.deserializeJavaObject(bytes, HyperellipticalWorldMap.class);
+            Assert.assertEquals(data, data2);
+        }
+    }
+    
+    @Test
+    public void testLatLonWorldMap() {
+        LoggerFactory.disableLogging();
+        Fury fury = Fury.builder().withLanguage(org.apache.fury.config.Language.JAVA).build();
+        fury.registerSerializer(LatLonWorldMap.class, new LatLonWorldMapSerializer(fury));
+
+        LatLonWorldMap data = new LatLonWorldMap(123, 200, 100);
+        data.generate();
+        {
+            byte[] bytes = fury.serializeJavaObject(data);
+            LatLonWorldMap data2 = fury.deserializeJavaObject(bytes, LatLonWorldMap.class);
+            Assert.assertEquals(data, data2);
+        }
+    }
+
+    @Test
+    public void testLocalMap() {
+        LoggerFactory.disableLogging();
+        Fury fury = Fury.builder().withLanguage(org.apache.fury.config.Language.JAVA).build();
+        fury.registerSerializer(LocalMap.class, new LocalMapSerializer(fury));
+
+        LocalMap data = new LocalMap(123, 100, 100);
+        data.generate();
+        {
+            byte[] bytes = fury.serializeJavaObject(data);
+            LocalMap data2 = fury.deserializeJavaObject(bytes, LocalMap.class);
+            Assert.assertEquals(data, data2);
+        }
+    }
+
+    @Test
+    public void testMimicLocalMap() {
+        LoggerFactory.disableLogging();
+        Fury fury = Fury.builder().withLanguage(org.apache.fury.config.Language.JAVA).build();
+        fury.registerSerializer(MimicLocalMap.class, new MimicLocalMapSerializer(fury));
+
+        MimicLocalMap data = new MimicLocalMap(123);
+        data.generate();
+        {
+            byte[] bytes = fury.serializeJavaObject(data);
+            MimicLocalMap data2 = fury.deserializeJavaObject(bytes, MimicLocalMap.class);
+            Assert.assertEquals(data, data2);
+        }
+    }
+
+    @Test
+    public void testMimicWorldMap() {
+        LoggerFactory.disableLogging();
+        Fury fury = Fury.builder().withLanguage(org.apache.fury.config.Language.JAVA).build();
+        fury.registerSerializer(MimicWorldMap.class, new MimicWorldMapSerializer(fury));
+
+        MimicWorldMap data = new MimicWorldMap(123);
+        data.generate();
+        {
+            byte[] bytes = fury.serializeJavaObject(data);
+            MimicWorldMap data2 = fury.deserializeJavaObject(bytes, MimicWorldMap.class);
+            Assert.assertEquals(data, data2);
+        }
+    }
+
+    @Test
+    public void testRotatingGlobeMap() {
+        LoggerFactory.disableLogging();
+        Fury fury = Fury.builder().withLanguage(org.apache.fury.config.Language.JAVA).build();
+        fury.registerSerializer(RotatingGlobeMap.class, new RotatingGlobeMapSerializer(fury));
+
+        RotatingGlobeMap data = new RotatingGlobeMap(123, 100, 100);
+        data.generate();
+        {
+            byte[] bytes = fury.serializeJavaObject(data);
+            RotatingGlobeMap data2 = fury.deserializeJavaObject(bytes, RotatingGlobeMap.class);
+            Assert.assertEquals(data, data2);
+        }
+    }
+
+    @Test
+    public void testRoundSideWorldMap() {
+        LoggerFactory.disableLogging();
+        Fury fury = Fury.builder().withLanguage(org.apache.fury.config.Language.JAVA).build();
+        fury.registerSerializer(RoundSideWorldMap.class, new RoundSideWorldMapSerializer(fury));
+
+        RoundSideWorldMap data = new RoundSideWorldMap(123, 100, 100);
+        data.generate();
+        {
+            byte[] bytes = fury.serializeJavaObject(data);
+            RoundSideWorldMap data2 = fury.deserializeJavaObject(bytes, RoundSideWorldMap.class);
+            Assert.assertEquals(data, data2);
+        }
+    }
+
+    @Test
+    public void testStretchWorldMap() {
+        LoggerFactory.disableLogging();
+        Fury fury = Fury.builder().withLanguage(org.apache.fury.config.Language.JAVA).build();
+        fury.registerSerializer(StretchWorldMap.class, new StretchWorldMapSerializer(fury));
+
+        StretchWorldMap data = new StretchWorldMap(123, 100, 100);
+        data.generate();
+        {
+            byte[] bytes = fury.serializeJavaObject(data);
+            StretchWorldMap data2 = fury.deserializeJavaObject(bytes, StretchWorldMap.class);
+            Assert.assertEquals(data, data2);
+        }
+    }
+
+    @Test
+    public void testTilingWorldMap() {
+        LoggerFactory.disableLogging();
+        Fury fury = Fury.builder().withLanguage(org.apache.fury.config.Language.JAVA).build();
+        fury.registerSerializer(TilingWorldMap.class, new TilingWorldMapSerializer(fury));
+
+        TilingWorldMap data = new TilingWorldMap(123, 100, 100);
+        data.generate();
+        {
+            byte[] bytes = fury.serializeJavaObject(data);
+            TilingWorldMap data2 = fury.deserializeJavaObject(bytes, TilingWorldMap.class);
             Assert.assertEquals(data, data2);
         }
     }
