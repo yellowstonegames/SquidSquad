804bb405d6fbf5d7b75a7a0c11f5688b094640cd
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-03-04 23:42:42 -0800

    With masking, normal R-S pairing works...
    It doesn't work without masking to 16-bit positive numbers first. Could cast to char?

--- squidgrid/src/test/java/com/github/yellowstonegames/grid/CoordTest.java
+++ squidgrid/src/test/java/com/github/yellowstonegames/grid/CoordTest.java
@@ -6,11 +6,24 @@ public class CoordTest {
     /**
      * All unique!
      */
-    public void testRosenbergStrongUniqueness() {
+    public void testSignedRosenbergStrongUniqueness() {
         long[] bits = new long[1<<26];
         for (int x = -32768; x <= 32767; x++) {
             for (int y = -32768; y <= 32767; y++) {
                 int index = Coord.signedRosenbergStrongHashCode(x, y);
+                if((bits[index>>>6] & (1L << index)) != 0) {
+                    throw new RuntimeException("Point at " + x + "," + y + " collided at index " + index);
+                }
+                bits[index>>>6] |= (1L << index);
+            }
+        }
+    }
+    public void testRosenbergStrongUniqueness() {
+        long[] bits = new long[1<<26];
+        for (int x = -32768; x <= 32767; x++) {
+            for (int y = -32768; y <= 32767; y++) {
+                int max = Math.max(x&0xFFFF, y&0xFFFF);
+                int index = (max * max + max + (x&0xFFFF) - (y&0xFFFF));
                 if((bits[index>>>6] & (1L << index)) != 0) {
                     throw new RuntimeException("Point at " + x + "," + y + " collided at index " + index);
                 }
@@ -86,7 +99,8 @@ public class CoordTest {
     }
 
     public static void main(String[] args) {
-//        new CoordTest().testRosenbergStrongUniqueness(); // passes!
+//        new CoordTest().testSignedRosenbergStrongUniqueness(); // passes!
+        new CoordTest().testRosenbergStrongUniqueness(); // passes!
 //        new CoordTest().testSignedCantorUniqueness(); // fails!
 //        new CoordTest().testLimitedSignedCantorUniqueness(16384); // passes!
 //        new CoordTest().testLimitedSignedCantorUniqueness(17000); // passes!
@@ -111,7 +125,7 @@ public class CoordTest {
 
 //        new CoordTest().testCantorUniqueness(); // fails!
 //        new CoordTest().testLimitedCantorUniqueness(0, 32767); // passes!
-        new CoordTest().testLimitedCantorUniqueness(0, 32768); // fails!
+//        new CoordTest().testLimitedCantorUniqueness(0, 32768); // fails!
 //        new CoordTest().testLimitedCantorUniqueness(-1, 1); // fails!
 //        new CoordTest().testLimitedCantorUniqueness(-4096, 4095); // fails!
 //        new CoordTest().testLimitedCantorUniqueness(-8192, 8191); // fails!
