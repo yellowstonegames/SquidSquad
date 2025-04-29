e98ee3f8339e786b35a6d3bf316e2683049675a6
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-04-28 22:42:11 -0700

    DungeonGridVFTest!

--- squidglyph/src/test/java/com/github/yellowstonegames/glyph/DungeonGridVFTest.java
+++ squidglyph/src/test/java/com/github/yellowstonegames/glyph/DungeonGridVFTest.java
@@ -33,12 +33,14 @@ import com.badlogic.gdx.utils.TimeUtils;
 import com.github.tommyettinger.digital.ArrayTools;
 import com.github.tommyettinger.ds.ObjectDeque;
 import com.github.tommyettinger.random.EnhancedRandom;
+import com.github.tommyettinger.random.LineWobble;
 import com.github.tommyettinger.random.WhiskerRandom;
 import com.github.tommyettinger.textra.Font;
 import com.github.tommyettinger.textra.KnownFonts;
 import com.github.yellowstonegames.core.DescriptiveColor;
 import com.github.yellowstonegames.core.DescriptiveColorRgb;
 import com.github.yellowstonegames.core.FullPalette;
+import com.github.yellowstonegames.glyph.rexpaint.XPIO;
 import com.github.yellowstonegames.grid.*;
 import com.github.yellowstonegames.path.DijkstraMap;
 import com.github.yellowstonegames.place.DungeonProcessor;
@@ -47,9 +49,10 @@ import static com.badlogic.gdx.Gdx.input;
 import static com.badlogic.gdx.Input.Keys.*;
 import static com.github.yellowstonegames.core.DescriptiveColor.*;
 
-public class LightingVFTest extends ApplicationAdapter {
+public class DungeonGridVFTest extends ApplicationAdapter {
 
     private Stage stage;
+    private Font font;
     private GlyphGrid gg;
     private DungeonProcessor dungeonProcessor;
     private char[][] bare;
@@ -60,17 +63,16 @@ public class LightingVFTest extends ApplicationAdapter {
     private final Noise waves = new Noise(123, 0.5f, Noise.FOAM, 1);
     private GlyphActor playerGlyph;
     private DijkstraMap playerToCursor;
-    private final ObjectDeque<Coord> toCursor =     new ObjectDeque<>(100);
+    private final ObjectDeque<Coord> toCursor = new ObjectDeque<>(100);
     private final ObjectDeque<Coord> awaitedMoves = new ObjectDeque<>(50);
     private Coord cursor = Coord.get(-1, -1);
     private final Vector2 pos = new Vector2();
-
     private long startTime, lastMove;
 
-    private static final int GRID_WIDTH = 75;
-    private static final int GRID_HEIGHT = 40;
-    private static final int CELL_WIDTH = 24;
-    private static final int CELL_HEIGHT = 24;
+    private static final int GRID_WIDTH = 40;
+    private static final int GRID_HEIGHT = 25;
+    private static final int CELL_WIDTH = 32;
+    private static final int CELL_HEIGHT = 32;
 
     private static final int DEEP_OKLAB = describeOklab("dark dull cobalt");
     private static final int SHALLOW_OKLAB = describeOklab("dull denim");
@@ -81,29 +83,43 @@ public class LightingVFTest extends ApplicationAdapter {
     private static final int shallowText = toRGBA8888(offsetLightness(SHALLOW_OKLAB));
     private static final int grassText = toRGBA8888(offsetLightness(GRASS_OKLAB));
     private static final int stoneText = toRGBA8888(describeOklab("gray dullmost butter bronze"));
-    private static final int SILVER_RGBA = toRGBA8888(SILVER);
-    private static final int MEMORY_RGBA = describe("darker gray black");
 
     public static void main(String[] args){
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-        config.setTitle("Lighting Test");
+        config.setTitle("Dungeon Map ('r' rebuilds!)");
         config.setWindowedMode(GRID_WIDTH * CELL_WIDTH, GRID_HEIGHT * CELL_HEIGHT);
         config.disableAudio(true);
-        config.setForegroundFPS(0); // how fast can this get, anyway?
-        config.useVsync(false);
-        new Lwjgl3Application(new LightingVFTest(), config);
+        config.setForegroundFPS(360); // shouldn't need to be any faster
+        config.useVsync(true);
+        new Lwjgl3Application(new DungeonGridVFTest(), config);
     }
 
     @Override
     public void create() {
         Gdx.app.setLogLevel(Application.LOG_INFO);
-        long seed = 123; // fixed seed every time
+        long seed = 12345; // unchanging seed
 //        long seed = EnhancedRandom.seedFromMath();// random seed every time
 //        long seed = TimeUtils.millis() >>> 21; // use to keep each seed for about half an hour; useful for testing
         Gdx.app.log("SEED", "Initial seed is " + seed);
         EnhancedRandom random = new WhiskerRandom(seed);
         stage = new Stage();
-        Font font = KnownFonts.getIosevka(Font.DistanceFieldType.SDF).multiplyCrispness(0.5f);
+//        Font font = KnownFonts.getIosevka().scaleTo(15f, 25f);
+        font = KnownFonts.getInconsolata(Font.DistanceFieldType.SDF);
+//        Font font = KnownFonts.getIosevka().scaleTo(15f, 25f).setDescent(0f).adjustLineHeight(1.25f);
+
+//        Font font = KnownFonts.getIosevkaSlab().scaleTo(15f, 25f);
+//        Font font = KnownFonts.getCascadiaMonoMSDF().scaleTo(15f, 25f);
+//        font = KnownFonts.getIosevka().scale(0.75f, 0.75f);
+//        Font font = KnownFonts.getIosevkaSlabSDF().scaleTo(12f, 26f);
+//        Font font = KnownFonts.getGoNotoUniversalSDF().scaleTo(30f, 25f).setCrispness(0.4f);
+//        Font font = KnownFonts.getInconsolata();
+//        font = KnownFonts.getDejaVuSansMono().scale(0.75f, 0.75f);
+//        Font font = KnownFonts.getCozette();
+//        Font font = KnownFonts.getAStarry();
+//        Font font = KnownFonts.getIosevkaMSDF().scaleTo(24, 24);
+//        Font font = KnownFonts.getAStarry().scaleTo(16, 16);
+//        Font font = KnownFonts.getAStarry().fitCell(24, 24, true);
+//        Font font = KnownFonts.getInconsolataMSDF().fitCell(24, 24, true);
         gg = new GlyphGrid(font, GRID_WIDTH, GRID_HEIGHT, true);
         //use Ă to test glyph height
         playerGlyph = new GlyphActor('@', "[red orange]", gg.font);
@@ -111,19 +127,9 @@ public class LightingVFTest extends ApplicationAdapter {
 
         vision.rememberedColor = DescriptiveColor.describeOklab("darker gray black");
 
-        //            lighting.calculateFOV(player.x, player.y, player.x - 10, player.y - 10, player.x + 11, player.y + 11);
-        //            blockage.refill(lighting.fovResult, 0f);
-        //            // blockage.not() flips its values so now it stores all cells that ARE visible in the latest lightLevels calc.
-        //            inView.remake(blockage.not());
-        //            // then, seen has all of those cells that have been visible (ever) included in with its cells.
-        //            seen.or(inView);
-        //            // changes blockage so instead of all currently visible cells, it now stores the cells that would have been
-        //            // adjacent to those cells.
-        //            blockage.fringe8way();
-        //            LineTools.pruneLines(dungeon, seen, prunedDungeon);
-
         dungeonProcessor = new DungeonProcessor(GRID_WIDTH, GRID_HEIGHT, random);
-        dungeonProcessor.addBoulders(DungeonProcessor.ALL, 3);
+        dungeonProcessor.addWater(DungeonProcessor.ALL, 30);
+        dungeonProcessor.addGrass(DungeonProcessor.ALL, 10);
         waves.setFractalType(Noise.RIDGED_MULTI);
         input.setInputProcessor(new InputAdapter(){
             @Override
@@ -135,6 +141,10 @@ public class LightingVFTest extends ApplicationAdapter {
                         break;
                     case R:
                         regenerate();
+                        break;
+                    case ENTER:
+                    case NUMPAD_ENTER:
+                        XPIO.saveXP(Gdx.files.local("Dungeon" + System.currentTimeMillis() + ".xp"), gg);
                         break;
                     default: return false;
                 }
@@ -169,7 +179,7 @@ public class LightingVFTest extends ApplicationAdapter {
                 gg.viewport.unproject(pos);
                 if (onGrid(screenX = MathUtils.floor(pos.x), screenY = MathUtils.floor(pos.y))) {
                     // we also need to check if screenX or screenY is the same cell.
-                    if (cursor.x == screenX && cursor.y == screenY) {
+                    if (cursor.x == screenX && cursor.y == screenY || gg.areChildrenActing()) {
                         return false;
                     }
                     cursor = Coord.get(screenX, screenY);
@@ -213,8 +223,8 @@ public class LightingVFTest extends ApplicationAdapter {
 //            if(MathUtils.randomBoolean())
                 playerGlyph.addAction(MoreActions.bump(way, 0.3f).append(MoreActions.wiggle(0.2f, 0.2f))
                                         .append(Actions.rotateBy(360f, 1f))
-//                        .append(new GridAction.PulseAction(gg, 1.5f, inView, next, 5).useMistyColors())
-                        );
+                        .append(new GridAction.ExplosionAction(gg, 1.5f, vision.inView, next, 5))
+                );
 //            else
 //                playerGlyph.addAction(MoreActions.bump(way, 0.3f).append(MoreActions.wiggle(0.2f, 0.2f))
 //                    .append(new GridAction.CloudAction(gg, 1.5f, inView, next, 5).useToxicColors()).conclude(post));
@@ -233,28 +243,13 @@ public class LightingVFTest extends ApplicationAdapter {
         char[][] dungeon;
         dungeonProcessor.setPlaceGrid(dungeon = LineTools.hashesToLines(dungeonProcessor.generate(), true));
         bare = dungeonProcessor.getBarePlaceGrid();
-        Region floors = new Region(bare, '.');
-        EnhancedRandom rng = dungeonProcessor.rng;
-        Coord player = floors.singleRandom(rng);
+        Coord player = new Region(bare, '.').singleRandom(dungeonProcessor.rng);
         playerGlyph.setPosition(player.x, player.y);
-
         vision.restart(dungeon, player, 8);
 
 //        vision.lighting.addLight(player, new Radiance(8, FullPalette.COSMIC_LATTE, 0f, 0f)); // constant light
-        vision.lighting.addLight(player, new Radiance(8, FullPalette.COSMIC_LATTE, 0.4f, 0f)); // flickers
-        floors.remove(player);
+        vision.lighting.addLight(player, new Radiance(8, FullPalette.SILVER_GREY, 0.4f, 0f)); // flickers
 
-        Coord[] lightPositions = floors.separatedBlue(0.075f);
-        for (int i = 0; i < lightPositions.length; i++) {
-            vision.lighting.addLight(lightPositions[i], new Radiance(rng.nextFloat(3f) + 2f,
-                    FullPalette.COLOR_WHEEL_PALETTE_BRIGHT[rng.nextInt(FullPalette.COLOR_WHEEL_PALETTE_BRIGHT.length)], 0.5f, 0f));
-        }
-//        lighting.calculateFOV(player.x, player.y, player.x - 10, player.y - 10, player.x + 11, player.y + 11);
-//        inView = inView == null ? new Region(lighting.fovResult, 0.01f, 2f) : inView.refill(lighting.fovResult, 0.01f, 2f);
-//        seen = seen == null ? inView.copy() : seen.remake(inView);
-//        blockage = blockage == null ? new Region(seen).fringe8way() : blockage.remake(seen).fringe8way();
-//        inView.remake(seen);
-//        LineTools.pruneLines(dungeon, seen, prunedDungeon);
         gg.backgrounds = new int[GRID_WIDTH][GRID_HEIGHT];
         gg.map.clear();
         if(playerToCursor == null)
@@ -270,29 +265,59 @@ public class LightingVFTest extends ApplicationAdapter {
     public void recolor(){
         float change = Math.min(Math.max(TimeUtils.timeSinceMillis(lastMove) * 4f, 0f), 1000f);
         vision.update(change);
-
         float modifiedTime = (TimeUtils.millis() & 0xFFFFFL) * 0x1p-9f;
+        // this could be used if you want the cursor highlight to be all one color.
 //        int rainbow = toRGBA8888(
 //                limitToGamut(100,
 //                        (int) (TrigTools.sinTurns(modifiedTime * 0.2f) * 40f) + 128, (int) (TrigTools.cosTurns(modifiedTime * 0.2f) * 40f) + 128, 255));
-//        FOV.reuseFOV(res, light, playerX, playerY, LineWobble.wobble(12345, modifiedTime) * 2.5f + 4f, Radius.CIRCLE);
+        char[][] prunedDungeon = vision.prunedPlaceMap;
+        float[][] light = vision.lighting.fovResult;
         ArrayTools.fill(gg.backgrounds, 0);
         for (int y = 0; y < GRID_HEIGHT; y++) {
             for (int x = 0; x < GRID_WIDTH; x++) {
                 if (vision.lighting.fovResult[x][y] > 0) {
-                    gg.put(x, y, vision.prunedPlaceMap[x][y], SILVER_RGBA);
-                    gg.backgrounds[x][y] = DescriptiveColor.toRGBA8888(vision.backgroundColors[x][y]);
-                }
-                else if (vision.seen.contains(x, y)) {
-                    gg.put(x, y, vision.prunedPlaceMap[x][y], SILVER_RGBA);
-                    gg.backgrounds[x][y] = MEMORY_RGBA;
-
+                    switch (prunedDungeon[x][y]) {
+                        case '~':
+                            gg.backgrounds[x][y] = toRGBA8888(DescriptiveColor.lerpColors(vision.backgroundColors[x][y], lighten(DEEP_OKLAB, 0.6f * Math.min(1.2f, Math.max(0, light[x][y] + waves.getConfiguredNoise(x, y, modifiedTime)))), 0.5f));
+                            gg.put(x, y, prunedDungeon[x][y], deepText);
+                            break;
+                        case ',':
+                            gg.backgrounds[x][y] = toRGBA8888(DescriptiveColor.lerpColors(vision.backgroundColors[x][y], lighten(SHALLOW_OKLAB, 0.6f * Math.min(1.2f, Math.max(0, light[x][y] + waves.getConfiguredNoise(x, y, modifiedTime)))), 0.5f));
+                            gg.put(x, y, prunedDungeon[x][y], shallowText);
+                            break;
+                        case '"':
+                            gg.backgrounds[x][y] = toRGBA8888(DescriptiveColor.lerpColors(vision.backgroundColors[x][y], darken(lerpColors(GRASS_OKLAB, DRY_OKLAB, waves.getConfiguredNoise(x, y) * 0.5f + 0.5f), 0.4f * Math.min(1.1f, Math.max(0, 1f - light[x][y] + waves.getConfiguredNoise(x, y, modifiedTime * 0.7f)))), 0.5f));
+                            gg.put(x, y, prunedDungeon[x][y], grassText);
+                            break;
+                        case ' ':
+                            break;
+                        default:
+                            gg.backgrounds[x][y] = toRGBA8888(DescriptiveColor.lerpColors(vision.backgroundColors[x][y], lighten(STONE_OKLAB, 0.6f * light[x][y]), 0.5f));
+                            gg.put(x, y, prunedDungeon[x][y], stoneText);
+                    }
+                } else if (vision.seen.contains(x, y)) {
+                    switch (prunedDungeon[x][y]) {
+                        case '~':
+                            gg.backgrounds[x][y] = toRGBA8888(vision.backgroundColors[x][y]);
+                            gg.put(x, y, prunedDungeon[x][y], deepText);
+                            break;
+                        case ',':
+                            gg.backgrounds[x][y] = toRGBA8888(vision.backgroundColors[x][y]);
+                            gg.put(x, y, prunedDungeon[x][y], shallowText);
+                            break;
+                        case ' ':
+                            break;
+                        default:
+                            gg.backgrounds[x][y] = toRGBA8888(vision.backgroundColors[x][y]);
+                            gg.put(x, y, prunedDungeon[x][y], stoneText);
+                    }
+                } else {
+                    gg.backgrounds[x][y] = 0;
                 }
             }
         }
         Coord loc = playerGlyph.getLocation();
         gg.put(loc.x, loc.y, 0L);
-//        vision.lighting.draw(gg.backgrounds);
         for (int i = 0; i < toCursor.size(); i++) {
             Coord curr = toCursor.get(i);
             if(vision.inView.contains(curr))
@@ -326,7 +351,6 @@ public class LightingVFTest extends ApplicationAdapter {
 
     @Override
     public void render() {
-
         if(!gg.areChildrenActing() && !awaitedMoves.isEmpty())
         {
             Coord m = awaitedMoves.removeFirst();
@@ -336,25 +360,25 @@ public class LightingVFTest extends ApplicationAdapter {
         }
         else if (!gg.areChildrenActing()) {
 //                postMove();
-            // this only happens if we just removed the last Coord from awaitedMoves, and it's only then that we need to
-            // re-calculate the distances from all cells to the player. We don't need to calculate this information on
-            // each part of a many-cell move (just the end), nor do we need to calculate it whenever the mouse moves.
-            if (awaitedMoves.isEmpty()) {
-                // the next two lines remove any lingering data needed for earlier paths
-                playerToCursor.clearGoals();
-                playerToCursor.resetMap();
-                // the next line marks the player as a "goal" cell, which seems counter-intuitive, but it works because all
-                // cells will try to find the distance between themselves and the nearest goal, and once this is found, the
-                // distances don't change as long as the goals don't change. Since the mouse will move and new paths will be
-                // found, but the player doesn't move until a cell is clicked, the "goal" is the non-changing cell, so the
-                // player's position, and the "target" of a pathfinding method like DijkstraMap.findPathPreScanned() is the
-                // currently-moused-over cell, which we only need to set where the mouse is being handled.
-                playerToCursor.setGoal(playerGlyph.getLocation());
-                // DijkstraMap.partialScan only finds the distance to get to a cell if that distance is less than some limit,
-                // which is 13 here. It also won't try to find distances through an impassable cell, which here is the blockage
-                // GreasedRegion that contains the cells just past the edge of the player's FOV area.
-                playerToCursor.partialScan(13, vision.blockage);
-            }
+                // this only happens if we just removed the last Coord from awaitedMoves, and it's only then that we need to
+                // re-calculate the distances from all cells to the player. We don't need to calculate this information on
+                // each part of a many-cell move (just the end), nor do we need to calculate it whenever the mouse moves.
+                if (awaitedMoves.isEmpty()) {
+                    // the next two lines remove any lingering data needed for earlier paths
+                    playerToCursor.clearGoals();
+                    playerToCursor.resetMap();
+                    // the next line marks the player as a "goal" cell, which seems counter-intuitive, but it works because all
+                    // cells will try to find the distance between themselves and the nearest goal, and once this is found, the
+                    // distances don't change as long as the goals don't change. Since the mouse will move and new paths will be
+                    // found, but the player doesn't move until a cell is clicked, the "goal" is the non-changing cell, so the
+                    // player's position, and the "target" of a pathfinding method like DijkstraMap.findPathPreScanned() is the
+                    // currently-moused-over cell, which we only need to set where the mouse is being handled.
+                    playerToCursor.setGoal(playerGlyph.getLocation());
+                    // DijkstraMap.partialScan only finds the distance to get to a cell if that distance is less than some limit,
+                    // which is 13 here. It also won't try to find distances through an impassable cell, which here is the blockage
+                    // GreasedRegion that contains the cells just past the edge of the player's FOV area.
+                    playerToCursor.partialScan(13, vision.blockage);
+                }
         } else {
             handleHeldKeys();
         }
