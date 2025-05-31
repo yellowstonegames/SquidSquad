9145aa81762a59b56c8a5ea9c1885a19a4fc00f6
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-05-31 02:15:23 -0700

    The rest of the WorldMap serializers.
    These still need tests, and the other types in squidworld need serialization, too (biome mappers and their views).

---
+++ squidfreeze/world/src/main/java/com/github/yellowstonegames/freeze/world/MimicWorldMapSerializer.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2020-2024 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.yellowstonegames.freeze.world;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import com.github.yellowstonegames.world.MimicWorldMap;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Serializer for {@link MimicWorldMap}; doesn't need anything else registered.
+ */
+public class MimicWorldMapSerializer extends Serializer<MimicWorldMap> {
+    public MimicWorldMapSerializer() {
+        setImmutable(false);
+        setAcceptsNull(false);
+    }
+
+    @Override
+    public void write(final Kryo kryo, final Output output, final MimicWorldMap data) {
+        String ser = data.stringSerialize();
+        byte[] byteStr = ser.getBytes(StandardCharsets.UTF_8);
+        output.writeInt(byteStr.length);
+        output.writeBytes(byteStr);
+    }
+
+    @Override
+    public MimicWorldMap read(final Kryo kryo, final Input input, final Class<? extends MimicWorldMap> dataClass) {
+        int len = input.readInt();
+        String str = new String(input.readBytes(len), StandardCharsets.UTF_8);
+        return MimicWorldMap.recreateFromString(str);
+    }
+}
