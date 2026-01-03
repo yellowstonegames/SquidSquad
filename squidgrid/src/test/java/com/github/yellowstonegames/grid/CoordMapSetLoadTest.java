5939521b005ab35f2070ecc0619092d5d500ecdf
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-01-02 19:42:18 -0800

    CoordSet is definitely fast! The rest, hm...
    ObjectSet is sometimes significantly faster than HashSet, but mostly when it doesn't need to resize. CoordSet is the fastest for Coord keys.

--- squidgrid/src/test/java/com/github/yellowstonegames/grid/CoordMapSetLoadTest.java
+++ squidgrid/src/test/java/com/github/yellowstonegames/grid/CoordMapSetLoadTest.java
@@ -19,45 +19,106 @@ package com.github.yellowstonegames.grid;
 import com.github.tommyettinger.ds.ObjectSet;
 import org.junit.Test;
 
+import java.util.HashSet;
+
 public class CoordMapSetLoadTest {
+    public static final int LIMIT = 100;
+    public static final int SIZE = 2048;
+    public static final int CAPACITY = SIZE * SIZE;
+    public static final float LOAD = 0.5f;
     /**
-     * Using new ObjectSet<>(51, 0.7f):
-     * Took 2628 ms.
-     * Using new ObjectSet<>(51, 0.5f):
-     * Took 3092 ms.
+     * Creating 100 sets with 4194304 Coord items each...
+     * ObjectSet took 54576 ms with CAPACITY=51 and LOAD=0.5
+     * <br>
+     * Creating 100 sets with 4194304 Coord items each...
+     * ObjectSet took 66185 ms with CAPACITY=51 and LOAD=0.9
+     * <br>
+     * Creating 100 sets with 4194304 Coord items each...
+     * ObjectSet took 49825 ms with CAPACITY=4194304 and LOAD=0.5
+     * <br>
+     * Creating 100 sets with 4194304 Coord items each...
+     * ObjectSet took 15660 ms with CAPACITY=4194304 and LOAD=0.9
      */
     @Test
     public void fillHugeObjectSetTest() {
         Coord.expandPoolTo(2048, 2048);
+        ObjectSet<Coord> set = ObjectSet.with(Coord.get(0,0));
+        System.out.println("Creating " + LIMIT + " sets with " + (SIZE * SIZE) + " Coord items each...");
         long startTime = System.currentTimeMillis();
-        ObjectSet<Coord> set = new ObjectSet<>(51, 0.5f);
-        for (int x = 0; x < 2048; x++) {
-            for (int y = 0; y < 2048; y++) {
-                set.add(Coord.get(x, y));
+        for (int i = 0; i < LIMIT; i++) {
+            set = new ObjectSet<>(CAPACITY, LOAD);
+            for (int x = 0; x < SIZE; x++) {
+                for (int y = 0; y < SIZE; y++) {
+                    set.add(Coord.get(x, y));
+                }
             }
         }
-        System.out.println("Took " + (System.currentTimeMillis() - startTime) + " ms.");
+        System.out.println("ObjectSet took " + (System.currentTimeMillis() - startTime) +
+                " ms with CAPACITY=" + CAPACITY + " and LOAD=" + LOAD);
         System.out.println(set.first());
     }
     /**
-     * Using new CoordSet(51, 0.7f), place() commented out:
-     * Took 2279 ms.
-     * Same as above, but place() intact:
-     * Took 2257 ms.
-     * Using new CoordSet(51, 0.5f), place() intact:
-     * Took 1929 ms.
+     * Creating 100 sets with 4194304 Coord items each...
+     * CoordSet took 36681 ms with CAPACITY=51 and LOAD=0.5
+     * <br>
+     * Creating 100 sets with 4194304 Coord items each...
+     * CoordSet took 31913 ms with CAPACITY=51 and LOAD=0.9
+     * <br>
+     * Creating 100 sets with 4194304 Coord items each...
+     * CoordSet took 26515 ms with CAPACITY=4194304 and LOAD=0.5
+     * <br>
+     * Creating 100 sets with 4194304 Coord items each...
+     * CoordSet took 14927 ms with CAPACITY=4194304 and LOAD=0.9
      */
     @Test
     public void fillHugeCoordSetTest() {
         Coord.expandPoolTo(2048, 2048);
+        CoordSet set = CoordSet.with(Coord.get(0,0));
+        System.out.println("Creating " + LIMIT + " sets with " + (SIZE * SIZE) + " Coord items each...");
         long startTime = System.currentTimeMillis();
-        CoordSet set = new CoordSet(51, 0.5f);
-        for (int x = 0; x < 2048; x++) {
-            for (int y = 0; y < 2048; y++) {
-                set.add(Coord.get(x, y));
+        for (int i = 0; i < LIMIT; i++) {
+            set = new CoordSet(CAPACITY, LOAD);
+            for (int x = 0; x < SIZE; x++) {
+                for (int y = 0; y < SIZE; y++) {
+                    set.add(Coord.get(x, y));
+                }
             }
         }
-        System.out.println("Took " + (System.currentTimeMillis() - startTime) + " ms.");
+        System.out.println("CoordSet took " + (System.currentTimeMillis() - startTime) +
+                " ms with CAPACITY=" + CAPACITY + " and LOAD=" + LOAD);
         System.out.println(set.first());
+    }
+
+    /**
+     * Creating 100 sets with 4194304 Coord items each...
+     * HashSet took 44468 ms with CAPACITY=51 and LOAD=0.5
+     * <br>
+     * Creating 100 sets with 4194304 Coord items each...
+     * HashSet took 58013 ms with CAPACITY=51 and LOAD=0.9
+     * <br>
+     * Creating 100 sets with 4194304 Coord items each...
+     * HashSet took 42864 ms with CAPACITY=4194304 and LOAD=0.5
+     * <br>
+     * Creating 100 sets with 4194304 Coord items each...
+     * HashSet took 48833 ms with CAPACITY=4194304 and LOAD=0.9
+     */
+    @Test
+    public void fillHugeHashSetTest() {
+        Coord.expandPoolTo(2048, 2048);
+        HashSet<Coord> set = new HashSet<>(1);
+        set.add(Coord.get(0,0));
+        System.out.println("Creating " + LIMIT + " sets with " + (SIZE * SIZE) + " Coord items each...");
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < LIMIT; i++) {
+            set = new HashSet<>(CAPACITY, LOAD);
+            for (int x = 0; x < SIZE; x++) {
+                for (int y = 0; y < SIZE; y++) {
+                    set.add(Coord.get(x, y));
+                }
+            }
+        }
+        System.out.println("HashSet took " + (System.currentTimeMillis() - startTime) +
+                " ms with CAPACITY=" + CAPACITY + " and LOAD=" + LOAD);
+        System.out.println(set.iterator().next());
     }
 }
