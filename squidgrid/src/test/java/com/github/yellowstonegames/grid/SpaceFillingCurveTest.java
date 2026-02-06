e7bc8786fc48b8bfa1c9f37f33ecef394cbb3a07
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-02-05 20:19:44 -0800

    Use reversible int for persInverse(); test; docs.
    There isn't much point in allowing a double for input if we can't get anything larger than an int from pers(), and pers() shouldn't produce a double anyway.

--- squidgrid/src/test/java/com/github/yellowstonegames/grid/SpaceFillingCurveTest.java
+++ squidgrid/src/test/java/com/github/yellowstonegames/grid/SpaceFillingCurveTest.java
@@ -182,16 +182,43 @@ public class SpaceFillingCurveTest {
         return (g * g * g) + m + w + (g + g + 1) * z;
     }
 
-    public static Point3Int persInverse(Point3Int p, double d){
+    /**
+     * Pigeon-Ettinger-Rosenberg-Strong Triple Inverse function.
+     * This takes a single double {@code d} for distance and a {@link Point3Int} {@code p} that will be modified
+     * in-place, and sets p to hold the coordinates of the point at distance {@code Math.floor(d)} along the
+     * Pigeon-Ettinger-Rosenberg-Strong space-filling curve, starting at the origin (0,0,0).
+     * The curve is organized into cube-shaped "shells" or "gnomons," each shell being completely returned before an
+     * outer shell can be returned. That is, the distance to a point inside a cube will always be less than the distance
+     * to a point that is outside that cube (if both have inputs that are in range). Two different shells will connect
+     * at one of two edges, either y=0,z=0 for an even shell connecting to a larger odd shell, or z=0,y=0 for an odd
+     * shell connecting to a larger even shell.
+     * <br>
+     * Notably, the distance will always be less than {@code Math.pow(Math.max(x, Math.max(y, z)) + 1, 3)} for inputs
+     * that are between 0 and 2146688999, inclusive.
+     * <br>
+     * This is based on a boustrophedonic Rosenberg-Strong pairing function (see
+     * <a href="https://hbfs.wordpress.com/2018/08/07/moeud-deux/">Steven Pigeon's blog</a> for more), but extended
+     * to three dimensions. The blog post suggests using the pair of x with the pair of y and z as a triple function,
+     * but that doesn't act like the 2D pairing function regarding shells.
+     * <br>
+     * "We could, of course, devise some complicated function for 3..." -- Steven Pigeon
+     * <br>
+     * "Yes! It works!" -- Tommy Ettinger
+     *
+     * @param p a non-null Point3Int that will be modified in-place
+     * @param d the distance along the space-filling curve from the origin, between 0 and 2146688999, inclusive
+     * @return {@code p}, containing the point at distance {@code d} along the curve
+     */
+    public static Point3Int persInverse(Point3Int p, int d){
         final int g = (int)(Math.cbrt(d)); // which gnomon, or shell, we are in
         final int u = g + 1;               // up one gnomon
-        final double b = (double) g * g * g;           // base of the current gnomon
+        final int b = g * g * g;           // base of the current gnomon
         if ((g & 1) == 0) {
-            if(d - b < (double) u * u)
+            if(d - b < u * u)
             {
                 // here we are on the top.
                 final int t = (int)Math.sqrt(d - b); // top gnomon
-                final int r = (int)(d - b - t * t);
+                final int r = d - b - t * t;
                 int i, j;
                 if(r <= t){
                     i = t;
@@ -204,9 +231,9 @@ public class SpaceFillingCurveTest {
                     p.set(i, j, g);
                 else p.set(j, i, g);
             } else {
-                // winding down.
-                int bend = g + g + 1;
-                int remain = (int)(d - b - (double) u * u); // remaining
+                // winding down
+                int bend = g + g + 1; // length of one bend; we will have g-many bends
+                int remain = d - b - u * u; // remaining after the top has been traversed
                 int ratio = remain / bend;
                 int k = g - 1 - ratio;
                 int i, j;
@@ -218,16 +245,17 @@ public class SpaceFillingCurveTest {
                     i = g + g - r;
                     j = g;
                 }
+                // alternate clockwise and counterclockwise winds
                 if((g + k & 1) == 1)
                     p.set(i, j, k);
                 else
                     p.set(j, i, k);
             }
         } else {
-            double lower = (double) u * u * g;
+            int lower = u * u * g;
             if(d >= lower){
-                // finishing at the top, wind back to x,y == 0,0 .
-                int top = (int) ((double)u * u * u - 1 - d);
+                // finishing at the top, wind back to x,y == 0,0
+                int top = u * u * u - 1 - d;
                 int t = (int) Math.sqrt(top); // top gnomon
                 int r = top - t * t;
                 int i, j;
@@ -242,9 +270,9 @@ public class SpaceFillingCurveTest {
                     p.set(i, j, g);
                 else p.set(j, i, g);
             } else {
-                // winding up.
-                int bend = g + g + 1;
-                int remain = (int)(d - b); // remaining
+                // winding up
+                int bend = g + g + 1; // length of one bend; we will have g-many bends
+                int remain = d - b; // remaining on the total winding section
                 int k = remain / bend;
                 int i, j;
                 int r = remain - k * bend;
@@ -255,6 +283,7 @@ public class SpaceFillingCurveTest {
                     i = g + g - r;
                     j = g;
                 }
+                // alternate clockwise and counterclockwise winds
                 if((g + k & 1) == 1)
                     p.set(i, j, k);
                 else
@@ -272,14 +301,31 @@ public class SpaceFillingCurveTest {
                 for (int k = 0; k < 100; k++) {
 //                    if((Math.max(i, Math.max(j, k)) & 1) == 1)
 //                        continue;
-                    double dist = pers(i, j, k);
+                    int dist = pers(i, j, k);
                     persInverse(store, dist);
-                    Assert.assertEquals("Failure at distance " + (int)dist, i, store.x);
-                    Assert.assertEquals("Failure at distance " + (int)dist, j, store.y);
-                    Assert.assertEquals("Failure at distance " + (int)dist, k, store.z);
+                    Assert.assertEquals("Failure at distance " + dist, i, store.x);
+                    Assert.assertEquals("Failure at distance " + dist, j, store.y);
+                    Assert.assertEquals("Failure at distance " + dist, k, store.z);
                 }
             }
         }
+        int limit = pers(1289, 1289, 1289);
+        persInverse(store, limit);
+        Assert.assertEquals("Failure at distance " + limit, 1289, store.x);
+        Assert.assertEquals("Failure at distance " + limit, 1289, store.y);
+        Assert.assertEquals("Failure at distance " + limit, 1289, store.z);
+
+        limit = pers(0, 0, 1289);
+        persInverse(store, limit);
+        Assert.assertEquals("Failure at distance " + limit, 0, store.x);
+        Assert.assertEquals("Failure at distance " + limit, 0, store.y);
+        Assert.assertEquals("Failure at distance " + limit, 1289, store.z);
+
+        limit = pers(1289, 0, 0);
+        persInverse(store, limit);
+        Assert.assertEquals("Failure at distance " + limit, 1289, store.x);
+        Assert.assertEquals("Failure at distance " + limit, 0, store.y);
+        Assert.assertEquals("Failure at distance " + limit, 0, store.z);
 
     }
 
