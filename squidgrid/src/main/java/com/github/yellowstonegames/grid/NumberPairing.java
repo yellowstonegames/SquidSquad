ceffdedc1ff626af6e72fc778ed49bc65e81a34c
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-02-09 00:23:15 -0800

    Finish the rest; document "jump" behavior.

--- squidgrid/src/main/java/com/github/yellowstonegames/grid/NumberPairing.java
+++ squidgrid/src/main/java/com/github/yellowstonegames/grid/NumberPairing.java
@@ -25,7 +25,9 @@ public final class NumberPairing {
      * 2D <a href="https://en.wikipedia.org/wiki/Pairing_function">Cantor pairing function</a>.
      * This is a way of getting a unique int result for small enough x and y values, where "small enough" can safely be
      * considered "between 0 and 32766." This can overflow if the sum of x and y is greater than 65533, so it can't
-     * reasonably deal with all int inputs.
+     * reasonably deal with all int inputs. This is permitted to have adjacent results "jump" from non-adjacent inputs,
+     * but only when the shell changes. The shell is defined as the sum of x and y. Adjacency here permits diagonal
+     * movement.
      *
      * @param x non-negative horizontal input, between 0 and 32766, inclusive
      * @param y non-negative vertical input, between 0 and 32766, inclusive
@@ -51,7 +53,8 @@ public final class NumberPairing {
     /**
      * 2D Rosenberg-Strong pairing function.
      * <a href="https://hbfs.wordpress.com/2018/08/07/moeud-deux/">See Steven Pigeon's blog</a>.
-     *
+     * This is permitted to have adjacent results "jump" from non-adjacent inputs, but only when the shell changes.
+     * The shell is defined as the greater of x and y. Adjacency here does not permit diagonal movement.
      * @param x non-negative horizontal input, between 0 and 46339, inclusive
      * @param y non-negative vertical input, between 0 and 46339, inclusive
      * @return pair of x and y as one int via the Rosenberg-Strong function
@@ -82,6 +85,238 @@ public final class NumberPairing {
             y = g;
         }
         return p.set(x, y);
+    }
+
+    /**
+     * 2D Boustrophedonic ("ox-turning", or "sidewinding") Rosenberg-Strong pairing function.
+     * <a href="https://hbfs.wordpress.com/2018/08/07/moeud-deux/">See Steven Pigeon's blog</a>.
+     * All adjacent results will come from adjacent inputs, even when the shell changes.
+     * The shell is defined as the greater of x and y. Adjacency here does not permit diagonal movement.
+     *
+     * @param x non-negative horizontal input, between 0 and 46339, inclusive
+     * @param y non-negative vertical input, between 0 and 46339, inclusive
+     * @return distance on the Boustrophedonic Rosenberg-Strong curve, from the origin
+     */
+    public static int windingRosenbergStrong(final int x, final int y){
+        final int m = Math.max(x, y);
+        final int s = -(m & 1);
+        return m * m + m + (x - y + s ^ s);
+    }
+
+    /**
+     * Inverse of the 2D Boustrophedonic ("ox-turning", or "sidewinding") Rosenberg-Strong pairing function.
+     * <a href="https://hbfs.wordpress.com/2018/08/07/moeud-deux/">See Steven Pigeon's blog</a>.
+     *
+     * @param p a point that will have its contents overwritten with the decoded x and y of the given distance
+     * @param d the distance on the Boustrophedonic Rosenberg-Strong curve, from the origin, between 0 and 2147395599, inclusive
+     * @return {@code p}, after modifications
+     */
+    public static Point2Int windingRosenbergStrongInverse(Point2Int p, int d){
+        final int g = (int)Math.sqrt(d);
+        final int r = d - g * g;
+        int i, j;
+        if(r <= g){
+            i = g;
+            j = r;
+        } else {
+            i = g + g - r;
+            j = g;
+        }
+        if((Math.max(i, j) & 1) == 1)
+            p.set(i, j);
+        else p.set(j, i);
+        return p;
+    }
+
+    /**
+     * 2D Szudzik Elegant pairing function.
+     * <a href="https://en.wikipedia.org/wiki/Pairing_function#Restriction_to_natural_numbers">See Wikipedia</a>.
+     * This pairing function does not guarantee that results in the same shell will be adjacent for adjacent inputs;
+     * that is, it is permitted to "jump" even within a shell.
+     *
+     * @param x non-negative horizontal input, between 0 and 46339, inclusive
+     * @param y non-negative vertical input, between 0 and 46339, inclusive
+     * @return pair of x and y as one int via the Szudzik Elegant function
+     */
+    public static int szudzik(final int x, final int y){
+        return x < y ? y * y + x : x * x + x + y;
+    }
+
+
+    /**
+     * Inverse of the 2D Szudzik Elegant pairing function.
+     * <a href="https://en.wikipedia.org/wiki/Pairing_function#Restriction_to_natural_numbers">See Wikipedia</a>.
+     *
+     * @param p a point that will have its contents overwritten with the decoded x and y of the given distance
+     * @param d pair of x and y via the Szudzik Elegant function, from the origin, between 0 and 2147395599, inclusive
+     * @return {@code p}, after modifications
+     */
+    public static Point2Int szudzikInverse(Point2Int p, int d) {
+        final int r = (int) Math.sqrt(d); // root
+        final int m = d - r * r;          // distance minus r squared
+        if(m < r){
+            p.set(m, r);
+        } else {
+            p.set(r, m - r);
+        }
+        return p;
+    }
+
+    /**
+     * 3D Pigeon-Ettinger-Rosenberg-Strong Triple (PERS) function.
+     * This gets a single integer that encodes three non-negative ints (between 0 and 1289, inclusive) into one distance
+     * along a space-filling curve starting at (0,0,0). The curve is organized into cube-shaped "shells" or "gnomons,"
+     * each shell being completely returned before an outer shell can be returned. That is, the distance to a point
+     * inside a cube will always be less than the distance to a point that is outside that cube (if both have inputs
+     * that are in range). Two different shells will connect at one of two edges, either y=0,z=0 for an even shell
+     * connecting to a larger odd shell, or z=0,y=0 for an odd shell connecting to a larger even shell.
+     * <br>
+     * Notably, the distance will always be less than {@code Math.pow(Math.max(x, Math.max(y, z)) + 1, 3)} for inputs
+     * that are in range. All adjacent results will come from adjacent inputs, even when the shell changes.
+     * The shell is defined as the greater of x, y, and z. Adjacency here does not permit diagonal movement.
+     * <br>
+     * This is based on a boustrophedonic Rosenberg-Strong pairing function (see
+     * <a href="https://hbfs.wordpress.com/2018/08/07/moeud-deux/">Steven Pigeon's blog</a> for more), but extended
+     * to three dimensions. The blog post suggests using the pair of x with the pair of y and z as a triple function,
+     * but that doesn't act like the 2D pairing function regarding shells.
+     * <br>
+     * "We could, of course, devise some complicated function for 3..." -- Steven Pigeon
+     * <br>
+     * "Yes! It works!" -- Tommy Ettinger
+     *
+     * @param x 3D coordinate, between 0 and 1289, inclusive
+     * @param y 3D coordinate, between 0 and 1289, inclusive
+     * @param z 3D coordinate, between 0 and 1289, inclusive
+     * @return the distance along a space-filling curve from the origin to the given point.
+     */
+    public static int pers(final int x, final int y, final int z) {
+        final int m = Math.max(x, y); // max of lateral dimensions
+        final int g = Math.max(m, z); // gnomon, or shell
+        final int u = g + 1;          // up one gnomon
+        final int s = -(m + z & 1);   // sign, used to flip direction
+        final int w = x - y + s ^ s;  // wind, used in all branches
+        if ((g & 1) == 0) {
+            // start at the top, wind over the top face, then snake down as z falls. Ends on x,y,z == g,0,0
+            if (g == z)
+                return (g * g * g) + m * m + m + w;
+            return (g * g * u) + m + w + (g + g + 1) * (g - z);
+        }
+        // start at the bottom, snake up as z rises, then wind over the top face until we reach x,y,z == 0,0,g
+        if (g == z) {
+            return (u * u * u - 1) - m * m - m + w;
+        }
+        return (g * g * g) + m + w + (g + g + 1) * z;
+    }
+
+    /**
+     * Inverse of the 3D Pigeon-Ettinger-Rosenberg-Strong (PERS) Triple function.
+     * This takes a single double {@code d} for distance and a {@link Point3Int} {@code p} that will be modified
+     * in-place, and sets p to hold the coordinates of the point at distance {@code Math.floor(d)} along the
+     * Pigeon-Ettinger-Rosenberg-Strong space-filling curve, starting at the origin (0,0,0).
+     * The curve is organized into cube-shaped "shells" or "gnomons," each shell being completely returned before an
+     * outer shell can be returned. That is, the distance to a point inside a cube will always be less than the distance
+     * to a point that is outside that cube (if both have inputs that are in range). Two different shells will connect
+     * at one of two edges, either y=0,z=0 for an even shell connecting to a larger odd shell, or z=0,y=0 for an odd
+     * shell connecting to a larger even shell.
+     * <br>
+     * Notably, the distance will always be less than {@code Math.pow(Math.max(x, Math.max(y, z)) + 1, 3)} for inputs
+     * that are between 0 and 2146688999, inclusive.
+     * <br>
+     * This is based on a boustrophedonic Rosenberg-Strong pairing function (see
+     * <a href="https://hbfs.wordpress.com/2018/08/07/moeud-deux/">Steven Pigeon's blog</a> for more), but extended
+     * to three dimensions. The blog post suggests using the pair of x with the pair of y and z as a triple function,
+     * but that doesn't act like the 2D pairing function regarding shells.
+     * <br>
+     * "We could, of course, devise some complicated function for 3..." -- Steven Pigeon
+     * <br>
+     * "Yes! It works!" -- Tommy Ettinger
+     *
+     * @param p a non-null Point3Int that will be modified in-place
+     * @param d the distance along the space-filling curve from the origin, between 0 and 2146688999, inclusive
+     * @return {@code p}, containing the point at distance {@code d} along the curve
+     */
+    public static Point3Int persInverse(Point3Int p, int d){
+        final int g = (int)(Math.cbrt(d)); // which gnomon, or shell, we are in
+        final int u = g + 1;               // up one gnomon
+        final int b = g * g * g;           // base of the current gnomon
+        if ((g & 1) == 0) {
+            if(d - b < u * u)
+            {
+                // here we are on the top.
+                final int t = (int)Math.sqrt(d - b); // top gnomon
+                final int r = d - b - t * t;
+                int i, j;
+                if(r <= t){
+                    i = t;
+                    j = r;
+                } else {
+                    i = t + t - r;
+                    j = t;
+                }
+                if((Math.max(i, j) & 1) == 1)
+                    p.set(i, j, g);
+                else p.set(j, i, g);
+            } else {
+                // winding down
+                int bend = g + g + 1; // length of one bend; we will have g-many bends
+                int remain = d - b - u * u; // remaining after the top has been traversed
+                int ratio = remain / bend;
+                int k = g - 1 - ratio;
+                int i, j;
+                int r = remain - ratio * bend;
+                if(r <= g){
+                    i = g;
+                    j = r;
+                } else {
+                    i = g + g - r;
+                    j = g;
+                }
+                // alternate clockwise and counterclockwise winds
+                if((g + k & 1) == 1)
+                    p.set(i, j, k);
+                else
+                    p.set(j, i, k);
+            }
+        } else {
+            int lower = u * u * g;
+            if(d >= lower){
+                // finishing at the top, wind back to x,y == 0,0
+                int top = u * u * u - 1 - d;
+                int t = (int) Math.sqrt(top); // top gnomon
+                int r = top - t * t;
+                int i, j;
+                if(r <= t){
+                    i = t;
+                    j = r;
+                } else {
+                    i = t + t - r;
+                    j = t;
+                }
+                if((Math.max(i, j) & 1) == 1)
+                    p.set(i, j, g);
+                else p.set(j, i, g);
+            } else {
+                // winding up
+                int bend = g + g + 1; // length of one bend; we will have g-many bends
+                int remain = d - b; // remaining on the total winding section
+                int k = remain / bend;
+                int i, j;
+                int r = remain - k * bend;
+                if(r <= g){
+                    i = g;
+                    j = r;
+                } else {
+                    i = g + g - r;
+                    j = g;
+                }
+                // alternate clockwise and counterclockwise winds
+                if((g + k & 1) == 1)
+                    p.set(i, j, k);
+                else
+                    p.set(j, i, k);
+            }
+        }
+        return p;
     }
 
 }
--- squidgrid/src/test/java/com/github/yellowstonegames/grid/SpaceFillingCurveTest.java
+++ squidgrid/src/test/java/com/github/yellowstonegames/grid/SpaceFillingCurveTest.java
@@ -98,78 +98,40 @@ public class SpaceFillingCurveTest {
         return rosenbergStrong(z, rosenbergStrong(x, y));
     }
 
-    /**
-     * 2D Boustrophedonic Rosenberg-Strong pairing function.
-     * <a href="https://hbfs.wordpress.com/2018/08/07/moeud-deux/">See Steven Pigeon's blog</a>.
-     *
-     * @param x non-negative horizontal input, between 0 and 46339, inclusive
-     * @param y non-negative vertical input, between 0 and 46339, inclusive
-     * @return distance on the Boustrophedonic Rosenberg-Strong curve, from the origin
-     */
-    public static int brs(final int x, final int y){
-        final int m = Math.max(x, y);
-        final int s = -(m & 1);
-        return m * m + m + (x - y + s ^ s);
-    }
-
-    /**
-     * Inverse of the 2D Boustrophedonic Rosenberg-Strong pairing function.
-     * <a href="https://hbfs.wordpress.com/2018/08/07/moeud-deux/">See Steven Pigeon's blog</a>.
-     *
-     * @param p a point that will have its contents overwritten with the decoded x and y of the given distance
-     * @param d the distance on the Boustrophedonic Rosenberg-Strong curve, from the origin, between 0 and 2147395599, inclusive
-     * @return {@code p}, after modifications
-     */
-    public static Point2Int brsInverse(Point2Int p, int d){
-        final int g = (int)Math.sqrt(d);
-        final int r = d - g * g;
-        int i, j;
-        if(r <= g){
-            i = g;
-            j = r;
-        } else {
-            i = g + g - r;
-            j = g;
-        }
-        if((Math.max(i, j) & 1) == 1)
-            p.set(i, j);
-        else p.set(j, i);
-        return p;
-    }
-
     @Test
     public void testBrs() {
         Point2Int store = new Point2Int();
         for (int i = 0; i < 100; i++) {
             for (int j = 0; j < 100; j++) {
-                int dist = brs(i, j);
-                brsInverse(store, dist);
+                int dist = windingRosenbergStrong(i, j);
+                windingRosenbergStrongInverse(store, dist);
                 Assert.assertEquals(i, store.x);
                 Assert.assertEquals(j, store.y);
             }
         }
         int limit;
 
-        limit = brs(46339, 46339);
-        brsInverse(store, limit);
+        limit = windingRosenbergStrong(46339, 46339);
+        windingRosenbergStrongInverse(store, limit);
 //        System.out.println(store + " has distance " + limit);
         Assert.assertEquals("Failure at distance " + limit, 46339, store.x);
         Assert.assertEquals("Failure at distance " + limit, 46339, store.y);
 
-        limit = brs(0, 46339);
-        brsInverse(store, limit);
+        limit = windingRosenbergStrong(0, 46339);
+        windingRosenbergStrongInverse(store, limit);
 //        System.out.println(store + " has distance " + limit);
         Assert.assertEquals("Failure at distance " + limit, 0, store.x);
         Assert.assertEquals("Failure at distance " + limit, 46339, store.y);
 
-        limit = brs(46339, 0);
-        brsInverse(store, limit);
+        limit = windingRosenbergStrong(46339, 0);
+        windingRosenbergStrongInverse(store, limit);
 //        System.out.println(store + " has distance " + limit);
         Assert.assertEquals("Failure at distance " + limit, 46339, store.x);
         Assert.assertEquals("Failure at distance " + limit, 0, store.y);
     }
 
     /**
+     * Attempt at a 3D Boustrophedonic Rosenberg-Strong triple function; doesn't work as hoped.
      * <pre>
      * 000 001 008 009
      * 003 002 007 010
@@ -197,163 +159,7 @@ public class SpaceFillingCurveTest {
      * @return
      */
     public static int brs(final int x, final int y, final int z){
-        return brs(x, brs(y, z));
-    }
-
-    /**
-     * 3D Pigeon-Ettinger-Rosenberg-Strong Triple function.
-     * This gets a single integer that encodes three non-negative ints (between 0 and 1289, inclusive) into one distance
-     * along a space-filling curve starting at (0,0,0). The curve is organized into cube-shaped "shells" or "gnomons,"
-     * each shell being completely returned before an outer shell can be returned. That is, the distance to a point
-     * inside a cube will always be less than the distance to a point that is outside that cube (if both have inputs
-     * that are in range). Two different shells will connect at one of two edges, either y=0,z=0 for an even shell
-     * connecting to a larger odd shell, or z=0,y=0 for an odd shell connecting to a larger even shell.
-     * <br>
-     * Notably, the distance will always be less than {@code Math.pow(Math.max(x, Math.max(y, z)) + 1, 3)} for inputs
-     * that are in range.
-     * <br>
-     * This is based on a boustrophedonic Rosenberg-Strong pairing function (see
-     * <a href="https://hbfs.wordpress.com/2018/08/07/moeud-deux/">Steven Pigeon's blog</a> for more), but extended
-     * to three dimensions. The blog post suggests using the pair of x with the pair of y and z as a triple function,
-     * but that doesn't act like the 2D pairing function regarding shells.
-     * <br>
-     * "We could, of course, devise some complicated function for 3..." -- Steven Pigeon
-     * <br>
-     * "Yes! It works!" -- Tommy Ettinger
-     *
-     * @param x 3D coordinate, between 0 and 1289, inclusive
-     * @param y 3D coordinate, between 0 and 1289, inclusive
-     * @param z 3D coordinate, between 0 and 1289, inclusive
-     * @return the distance along a space-filling curve from the origin to the given point.
-     */
-    public static int pers(final int x, final int y, final int z) {
-        final int m = Math.max(x, y); // max of lateral dimensions
-        final int g = Math.max(m, z); // gnomon, or shell
-        final int u = g + 1;          // up one gnomon
-        final int s = -(m + z & 1);   // sign, used to flip direction
-        final int w = x - y + s ^ s;  // wind, used in all branches
-        if ((g & 1) == 0) {
-            // start at the top, wind over the top face, then snake down as z falls. Ends on x,y,z == g,0,0
-            if (g == z)
-                return (g * g * g) + m * m + m + w;
-            return (g * g * u) + m + w + (g + g + 1) * (g - z);
-        }
-        // start at the bottom, snake up as z rises, then wind over the top face until we reach x,y,z == 0,0,g
-        if (g == z) {
-            return (u * u * u - 1) - m * m - m + w;
-        }
-        return (g * g * g) + m + w + (g + g + 1) * z;
-    }
-
-    /**
-     * Inverse of the 3D Pigeon-Ettinger-Rosenberg-Strong Triple function.
-     * This takes a single double {@code d} for distance and a {@link Point3Int} {@code p} that will be modified
-     * in-place, and sets p to hold the coordinates of the point at distance {@code Math.floor(d)} along the
-     * Pigeon-Ettinger-Rosenberg-Strong space-filling curve, starting at the origin (0,0,0).
-     * The curve is organized into cube-shaped "shells" or "gnomons," each shell being completely returned before an
-     * outer shell can be returned. That is, the distance to a point inside a cube will always be less than the distance
-     * to a point that is outside that cube (if both have inputs that are in range). Two different shells will connect
-     * at one of two edges, either y=0,z=0 for an even shell connecting to a larger odd shell, or z=0,y=0 for an odd
-     * shell connecting to a larger even shell.
-     * <br>
-     * Notably, the distance will always be less than {@code Math.pow(Math.max(x, Math.max(y, z)) + 1, 3)} for inputs
-     * that are between 0 and 2146688999, inclusive.
-     * <br>
-     * This is based on a boustrophedonic Rosenberg-Strong pairing function (see
-     * <a href="https://hbfs.wordpress.com/2018/08/07/moeud-deux/">Steven Pigeon's blog</a> for more), but extended
-     * to three dimensions. The blog post suggests using the pair of x with the pair of y and z as a triple function,
-     * but that doesn't act like the 2D pairing function regarding shells.
-     * <br>
-     * "We could, of course, devise some complicated function for 3..." -- Steven Pigeon
-     * <br>
-     * "Yes! It works!" -- Tommy Ettinger
-     *
-     * @param p a non-null Point3Int that will be modified in-place
-     * @param d the distance along the space-filling curve from the origin, between 0 and 2146688999, inclusive
-     * @return {@code p}, containing the point at distance {@code d} along the curve
-     */
-    public static Point3Int persInverse(Point3Int p, int d){
-        final int g = (int)(Math.cbrt(d)); // which gnomon, or shell, we are in
-        final int u = g + 1;               // up one gnomon
-        final int b = g * g * g;           // base of the current gnomon
-        if ((g & 1) == 0) {
-            if(d - b < u * u)
-            {
-                // here we are on the top.
-                final int t = (int)Math.sqrt(d - b); // top gnomon
-                final int r = d - b - t * t;
-                int i, j;
-                if(r <= t){
-                    i = t;
-                    j = r;
-                } else {
-                    i = t + t - r;
-                    j = t;
-                }
-                if((Math.max(i, j) & 1) == 1)
-                    p.set(i, j, g);
-                else p.set(j, i, g);
-            } else {
-                // winding down
-                int bend = g + g + 1; // length of one bend; we will have g-many bends
-                int remain = d - b - u * u; // remaining after the top has been traversed
-                int ratio = remain / bend;
-                int k = g - 1 - ratio;
-                int i, j;
-                int r = remain - ratio * bend;
-                if(r <= g){
-                    i = g;
-                    j = r;
-                } else {
-                    i = g + g - r;
-                    j = g;
-                }
-                // alternate clockwise and counterclockwise winds
-                if((g + k & 1) == 1)
-                    p.set(i, j, k);
-                else
-                    p.set(j, i, k);
-            }
-        } else {
-            int lower = u * u * g;
-            if(d >= lower){
-                // finishing at the top, wind back to x,y == 0,0
-                int top = u * u * u - 1 - d;
-                int t = (int) Math.sqrt(top); // top gnomon
-                int r = top - t * t;
-                int i, j;
-                if(r <= t){
-                    i = t;
-                    j = r;
-                } else {
-                    i = t + t - r;
-                    j = t;
-                }
-                if((Math.max(i, j) & 1) == 1)
-                    p.set(i, j, g);
-                else p.set(j, i, g);
-            } else {
-                // winding up
-                int bend = g + g + 1; // length of one bend; we will have g-many bends
-                int remain = d - b; // remaining on the total winding section
-                int k = remain / bend;
-                int i, j;
-                int r = remain - k * bend;
-                if(r <= g){
-                    i = g;
-                    j = r;
-                } else {
-                    i = g + g - r;
-                    j = g;
-                }
-                // alternate clockwise and counterclockwise winds
-                if((g + k & 1) == 1)
-                    p.set(i, j, k);
-                else
-                    p.set(j, i, k);
-            }
-        }
-        return p;
+        return windingRosenbergStrong(x, windingRosenbergStrong(y, z));
     }
 
     @Test
@@ -397,66 +203,33 @@ public class SpaceFillingCurveTest {
 //        System.out.println(store + " has distance " + limit);
     }
 
-
-    /**
-     * 2D Szudzik Elegant pairing function.
-     * <a href="https://en.wikipedia.org/wiki/Pairing_function#Restriction_to_natural_numbers">See Wikipedia</a>.
-     *
-     * @param x non-negative horizontal input, between 0 and 46339, inclusive
-     * @param y non-negative vertical input, between 0 and 46339, inclusive
-     * @return pair of x and y as one int via the Szudzik Elegant function
-     */
-    public static int se(final int x, final int y){
-        return x < y ? y * y + x : x * x + x + y;
-    }
-
-
-    /**
-     * Inverse of the 2D Szudzik Elegant pairing function.
-     * <a href="https://en.wikipedia.org/wiki/Pairing_function#Restriction_to_natural_numbers">See Wikipedia</a>.
-     *
-     * @param p a point that will have its contents overwritten with the decoded x and y of the given distance
-     * @param d pair of x and y via the Szudzik Elegant function, from the origin, between 0 and 2147395599, inclusive
-     * @return {@code p}, after modifications
-     */
-    public static Point2Int seInverse(Point2Int p, int d) {
-        final int r = (int) Math.sqrt(d); // root
-        final int m = d - r * r;          // distance minus r squared
-        if(m < r){
-            p.set(m, r);
-        } else {
-            p.set(r, m - r);
-        }
-        return p;
-    }
-
     @Test
     public void testSe() {
         Point2Int store = new Point2Int();
         for (int i = 0; i < 100; i++) {
             for (int j = 0; j < 100; j++) {
-                int dist = se(i, j);
-                seInverse(store, dist);
+                int dist = szudzik(i, j);
+                szudzikInverse(store, dist);
                 Assert.assertEquals(i, store.x);
                 Assert.assertEquals(j, store.y);
             }
         }
         int limit;
 
-        limit = se(46339, 46339);
-        seInverse(store, limit);
+        limit = szudzik(46339, 46339);
+        szudzikInverse(store, limit);
 //        System.out.println(store + " has distance " + limit);
         Assert.assertEquals("Failure at distance " + limit, 46339, store.x);
         Assert.assertEquals("Failure at distance " + limit, 46339, store.y);
 
-        limit = se(0, 46339);
-        seInverse(store, limit);
+        limit = szudzik(0, 46339);
+        szudzikInverse(store, limit);
 //        System.out.println(store + " has distance " + limit);
         Assert.assertEquals("Failure at distance " + limit, 0, store.x);
         Assert.assertEquals("Failure at distance " + limit, 46339, store.y);
 
-        limit = se(46339, 0);
-        seInverse(store, limit);
+        limit = szudzik(46339, 0);
+        szudzikInverse(store, limit);
 //        System.out.println(store + " has distance " + limit);
         Assert.assertEquals("Failure at distance " + limit, 46339, store.x);
         Assert.assertEquals("Failure at distance " + limit, 0, store.y);
