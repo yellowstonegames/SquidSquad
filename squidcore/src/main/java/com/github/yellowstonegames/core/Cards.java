f5a389193583e63f62fc516046d5be07b469561f
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-02-12 19:18:19 -0800

    Add some docs to Cards.

--- squidcore/src/main/java/com/github/yellowstonegames/core/Cards.java
+++ squidcore/src/main/java/com/github/yellowstonegames/core/Cards.java
@@ -5,6 +5,15 @@ import com.github.tommyettinger.ds.IntDeque;
 import com.github.tommyettinger.random.EnhancedRandom;
 import com.github.yellowstonegames.core.annotations.Beta;
 
+/**
+ * A simple class to simulate a deck of cards that can be shuffled or drawn from (without replacement). If the deck is
+ * empty and the user tries to draw a card, the deck is shuffled entirely and then a card is drawn. You can draw a card
+ * simulated by an int index with {@link #drawInt()}, or get a String name for a card drawn with {@link #drawName()}.
+ * <br>
+ * An array of String names must be provided (currently) for each card's name, such as "Ace of Spades" or "The Chariot"
+ * (for a game like poker, and for tarot, respectively). You can get a name from an int retrieved earlier with
+ * {@link #nameForInt(int)}, or just retrieve the name directly with {@link #drawName()}.
+ */
 @Beta
 public class Cards {
     public IntDeque deck;
@@ -17,6 +26,11 @@ public class Cards {
         this.names = names;
     }
 
+    /**
+     * If {@code full} is true, shuffles the entire deck back in to restart it from its original size in a new order.
+     * Otherwise, this only shuffles the remaining cards in the deck into a new order.
+     * @param full if true, the deck will start again at its original size; if false, only the remaining cards will be shuffled
+     */
     public void shuffleDeck(boolean full) {
         if(full){
             deck.clear();
@@ -37,8 +51,16 @@ public class Cards {
         return names[drawInt()];
     }
 
+    /**
+     * @param index typically returned by {@link #drawInt()}
+     * @return the name associated with {@code index}, or null if the index is invalid.
+     */
     public String nameForInt(int index) {
         if(index < 0 || index >= names.length) return null;
         return names[index];
+    }
+
+    public int remainingCards() {
+        return deck.size();
     }
 }
