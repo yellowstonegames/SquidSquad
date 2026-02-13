308dc629e2f1d3ca0c5271c9cc40de9c3d021132
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-02-12 20:08:09 -0800

    Document Cards quite a bit.

--- squidcore/src/main/java/com/github/yellowstonegames/core/Cards.java
+++ squidcore/src/main/java/com/github/yellowstonegames/core/Cards.java
@@ -20,7 +20,15 @@ import java.util.Arrays;
 @Beta
 public class Cards {
 
+    /**
+     * Predefined common types of card decks, stored by the names of cards (permitting duplicates).
+     */
     public enum DeckType {
+        /**
+         * The 22 major arcana cards in many tarot decks, typically used for fortune-telling and symbolism.
+         * These are sometimes numbered; the first card, The Fool, is numbered 0 or 22, and the rest usually have
+         * numbers equivalent to their indices here.
+         */
         TAROT_MAJOR_ARCANA(
                 "The Fool", "The Magician", "The High Priestess", "The Empress",
                 "The Emperor", "The Hierophant", "The Lovers", "The Chariot",
@@ -35,10 +43,20 @@ public class Cards {
             this.names = names;
         }
 
+        /**
+         * Returns the size of the deck of cards before any cards have been drawn.
+         * @return the size of the deck of cards before any cards have been drawn
+         */
         public int size() {
             return names.length;
         }
 
+        /**
+         * Tries to get the card with the given index in this DeckType, if the index is valid. Returns null if
+         * the index is negative or too large.
+         * @param index should be non-negative and less than {@link #size()}
+         * @return the card name with the given index
+         */
         public String get(int index) {
             if(index < 0 || index > names.length) return null;
             return names[index];
@@ -49,16 +67,50 @@ public class Cards {
     public EnhancedRandom random;
     public String[] names;
 
+    /**
+     * Creates a new Cards using the given array of names (which will be used directly, permitting the name of a card
+     * to potentially be changed later if the array is modified) and an {@link EnhancedRandom} generator.
+     * <br>
+     * If the {@link EnhancedRandom#getMinimumPeriod()} of the generator is at least equal to the
+     * {@link com.github.tommyettinger.digital.MathTools#factorial(double)} of {@code names.length}, then the
+     * generator will (in theory) be able to produce all possible shuffles of that deck. A generator such as
+     * {@link com.github.tommyettinger.random.OrbitalRandom} with a period of 2 to the 71 or greater can produce all
+     * shuffles for the 22 tarot major arcana. Larger decks might need as much as a period of 2 to the
+     * 256 for a typical playing card deck (with or without jokers). CCG-type decks with 60 or more cards likely can't
+     * have all shuffles produced by a single generator currently, though the amount of possible shuffles for even the
+     * 22 tarot major arcana cards is astronomically large already.
+     * @param names a String array of names for cards, which do not need to be unique; used directly (not copied)
+     * @param random an EnhancedRandom generator that will be used directly (not copied)
+     */
     public Cards(String[] names, EnhancedRandom random) {
         deck = new IntDeque(ArrayTools.range(names.length));
         this.random = random;
         this.names = names;
     }
 
+    /**
+     * Creates a new Cards using the specified {@link DeckType} and the given {@link EnhancedRandom} generator.
+     * <br>
+     * If the {@link EnhancedRandom#getMinimumPeriod()} of the generator is at least equal to the
+     * {@link com.github.tommyettinger.digital.MathTools#factorial(double)} of {@link DeckType#size()}, then the
+     * generator will (in theory) be able to produce all possible shuffles of that deck type. A generator such as
+     * {@link com.github.tommyettinger.random.OrbitalRandom} with a period of 2 to the 71 or greater can produce all
+     * shuffles for {@link DeckType#TAROT_MAJOR_ARCANA}. Larger decks might need as much as a period of 2 to the
+     * 256 for a typical playing card deck (with or without jokers). CCG-type decks with 60 or more cards likely can't
+     * have all shuffles produced by a single generator currently, though the amount of possible shuffles for even the
+     * 22 tarot major arcana cards is astronomically large already.
+     *
+     * @param type a predefined {@link DeckType} enum constant
+     */
     public Cards(DeckType type, EnhancedRandom random) {
         this(Arrays.copyOf(type.names, type.names.length), random);
     }
 
+    /**
+     * Creates a new Cards using the specified {@link DeckType} and an unseeded {@link Xoshiro256MX3Random} generator,
+     * which is guaranteed to be able to produce all possible shuffles of an up-to-57-card deck.
+     * @param type a predefined {@link DeckType} enum constant
+     */
     public Cards(DeckType type) {
         this(type, new Xoshiro256MX3Random());
     }
@@ -78,12 +130,24 @@ public class Cards {
         deck.shuffle(random);
     }
 
+    /**
+     * If the deck has any remaining cards, this removes the last card from the deck and returns its index.
+     * Otherwise, this shuffles the entire deck back up to its original size, and then removes the last card and
+     * returns its index.
+     * @return an index of a card in the deck
+     */
     public int drawInt() {
         if(deck.isEmpty())
             shuffleDeck(true);
         return deck.pop();
     }
 
+    /**
+     * If the deck has any remaining cards, this removes the last card from the deck and returns its name.
+     * Otherwise, this shuffles the entire deck back up to its original size, and then removes the last card and
+     * returns its name.
+     * @return a String name of a card in the deck
+     */
     public String drawName() {
         return names[drawInt()];
     }
@@ -97,6 +161,10 @@ public class Cards {
         return names[index];
     }
 
+    /**
+     * Gets how many cards are left in the deck, before drawing a card would need to shuffle.
+     * @return how many cards are left in the deck
+     */
     public int remainingCards() {
         return deck.size();
     }
