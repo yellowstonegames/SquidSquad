89c684c3c1436ffe5606bc8e16fbdb76cba80a20
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-06-04 21:15:14 -0700

    Test MarkovTextLimited; it works as...
    ... well as you would expect an order-1 Markov chain to do.

--- squidtext/src/test/java/com/github/yellowstonegames/text/MarkovTest.java
+++ squidtext/src/test/java/com/github/yellowstonegames/text/MarkovTest.java
@@ -68,6 +68,42 @@ public class MarkovTest {
         }
     }
 
+    @Test
+    public void testMarkovTextLimited() {
+        if(!TextInternals.PRINTING) return;
+        long seed = 123456789L;
+        String oz = "Dorothy lived in the midst of the great Kansas prairies, with Uncle Henry, who was a " +
+                "farmer, and Aunt Em, who was the farmer's wife. Their house was small, for the " +
+                "lumber to build it had to be carried by wagon many miles. There were four walls, " +
+                "a floor and a roof, which made one room; and this room contained a rusty looking " +
+                "cookstove, a cupboard for the dishes, a table, three or four chairs, and the beds. " +
+                "Uncle Henry and Aunt Em had a big bed in one corner, and Dorothy a little bed in " +
+                "another corner. There was no garret at all, and no cellar-except a small hole dug " +
+                "in the ground, called a cyclone cellar, where the family could go in case one of " +
+                "those great whirlwinds arose, mighty enough to crush any building in its path. It " +
+                "was reached by a trap door in the middle of the floor, from which a ladder led " +
+                "down into the small, dark hole. When Dorothy stood in the doorway and looked around, " +
+                "she could see nothing but the great gray prairie on every side. Not a tree nor a house " +
+                "broke the broad sweep of flat country that reached to the edge of the sky in all directions. " +
+                "The sun had baked the plowed land into a gray mass, with little cracks running through it. " +
+                "Even the grass was not green, for the sun had burned the tops of the long blades until they " +
+                "were the same gray color to be seen everywhere. Once the house had been painted, but the sun " +
+                "blistered the paint and the rains washed it away, and now the house was as dull and gray as " +
+                "everything else.";
+        MarkovTextLimited markovText = new MarkovTextLimited();
+        markovText.analyze(oz);
+        for (int i = 0; i < 40; i++) {
+            System.out.println(markovText.chain(++seed, 100 + (i * 2)));
+        }
+        seed = 123456789L;
+        System.out.println();
+        Translator cipher = new Translator(Language.JAPANESE_ROMANIZED);
+        markovText.changeNames(cipher);
+        for (int i = 0; i < 40; i++) {
+            System.out.println(markovText.chain(++seed, 100 + (i * 2)));
+        }
+    }
+
     /**
      * I should give credit where it is due; goetia.txt is from
      * <a href="https://en.wikisource.org/wiki/The_Lesser_Key_of_Solomon#Shemhamphorash">The Lesser Key of Solomon</a>,
