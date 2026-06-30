1e1106b1c664ca6da275c2a54cadef7db409c10b
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-06-30 03:12:53 -0700

    Hard to beat a good hash at this!

--- squidworld/src/test/java/com/github/yellowstonegames/world/HashFloodResistance.java
+++ squidworld/src/test/java/com/github/yellowstonegames/world/HashFloodResistance.java
@@ -17,6 +17,8 @@
 package com.github.yellowstonegames.world;
 
 
+import com.github.tommyettinger.digital.Hasher;
+
 import java.util.Collections;
 import java.util.List;
 
@@ -33,22 +35,22 @@ public class HashFloodResistance {
 //        com.github.tommyettinger.ds.ObjectSet<List<String>> set = new com.github.tommyettinger.ds.ObjectSet<>(100000); // 46.996 seconds taken.
 //        com.badlogic.gdx.utils.ObjectSet<List<String>> set = new com.badlogic.gdx.utils.ObjectSet<>(1); // 37.43 seconds taken.
 //        com.badlogic.gdx.utils.ObjectSet<List<String>> set = new com.badlogic.gdx.utils.ObjectSet<>(100000); // 37.068 seconds taken.
-        {
-            // testing 1 to 30000 NUL chars in a set of Lists:
-//            java.util.HashSet<List<String>> set = new java.util.HashSet<>(1, 0.75f); // 12.206 seconds taken.
-//            java.util.HashSet<List<String>> set = new java.util.HashSet<>(30000, 0.75f); // 13.275 seconds taken.
-//            com.github.tommyettinger.ds.ObjectSet<List<String>> set = new com.github.tommyettinger.ds.ObjectSet<>(1, 0.75f); // 4.127 seconds taken.
-//            com.github.tommyettinger.ds.ObjectSet<List<String>> set = new com.github.tommyettinger.ds.ObjectSet<>(30000, 0.75f); // 3.774 seconds taken.
-//            com.badlogic.gdx.utils.ObjectSet<List<String>> set = new com.badlogic.gdx.utils.ObjectSet<>(1, 0.75f); // 3.594 seconds taken.
-            com.badlogic.gdx.utils.ObjectSet<List<String>> set = new com.badlogic.gdx.utils.ObjectSet<>(30000, 0.75f); // 3.282 seconds taken.
-            String s = "\0";
-            for (int iter = 0; iter < 30; iter++) {
-                for (int i = 0; i < 1000; i++)
-                    set.add(Collections.singletonList(s += '\0'));
-            }
-            System.out.println(set.size);
-            System.out.println(((System.currentTimeMillis() - startTime) * 1E-3) + " seconds taken.");
-        }
+//        {
+//            // testing 1 to 30000 NUL chars in a set of Lists:
+////            java.util.HashSet<List<String>> set = new java.util.HashSet<>(1, 0.75f); // 12.206 seconds taken.
+////            java.util.HashSet<List<String>> set = new java.util.HashSet<>(30000, 0.75f); // 13.275 seconds taken.
+////            com.github.tommyettinger.ds.ObjectSet<List<String>> set = new com.github.tommyettinger.ds.ObjectSet<>(1, 0.75f); // 4.127 seconds taken.
+////            com.github.tommyettinger.ds.ObjectSet<List<String>> set = new com.github.tommyettinger.ds.ObjectSet<>(30000, 0.75f); // 3.774 seconds taken.
+////            com.badlogic.gdx.utils.ObjectSet<List<String>> set = new com.badlogic.gdx.utils.ObjectSet<>(1, 0.75f); // 3.594 seconds taken.
+//            com.badlogic.gdx.utils.ObjectSet<List<String>> set = new com.badlogic.gdx.utils.ObjectSet<>(30000, 0.75f); // 3.282 seconds taken.
+//            String s = "\0";
+//            for (int iter = 0; iter < 30; iter++) {
+//                for (int i = 0; i < 1000; i++)
+//                    set.add(Collections.singletonList(s += '\0'));
+//            }
+//            System.out.println(set.size);
+//            System.out.println(((System.currentTimeMillis() - startTime) * 1E-3) + " seconds taken.");
+//        }
 //        {
 //            // testing aaaaaaaaaaaa... up to 100000 chars per String:
 //            java.util.HashSet<List<String>> set = new java.util.HashSet<>(1, 0.75f); // 6.558 seconds taken.
@@ -65,5 +67,30 @@ public class HashFloodResistance {
 //            System.out.println(set.size());
 //            System.out.println(((System.currentTimeMillis() - startTime) * 1E-3) + " seconds taken.");
 //        }
+
+        {
+            // testing 1 to 30000 NUL chars in a set of Strings:
+            // HashSet takes advantage of String being Comparable and falls back to a TreeSet.
+//            java.util.HashSet<String> set = new java.util.HashSet<>(1, 0.75f); // 1.063 seconds taken.
+//            java.util.HashSet<String> set = new java.util.HashSet<>(30000, 0.75f); // 0.983 seconds taken.
+//            com.github.tommyettinger.ds.ObjectSet<String> set = new com.github.tommyettinger.ds.ObjectSet<>(1, 0.75f); // 3.024 seconds taken.
+//            com.github.tommyettinger.ds.ObjectSet<String> set = new com.github.tommyettinger.ds.ObjectSet<>(30000, 0.75f); // 2.949 seconds taken.
+            com.github.tommyettinger.ds.ObjectSet<String> set = new com.github.tommyettinger.ds.ObjectSet<String>(30000, 0.75f){
+                @Override
+                protected int place(Object item) {
+                    return Hasher.haagenti.hashBulk((String) item) & mask;
+                }
+            };// 0.75 seconds taken.
+//            com.badlogic.gdx.utils.ObjectSet<String> set = new com.badlogic.gdx.utils.ObjectSet<>(1, 0.75f); // 2.677 seconds taken.
+//            com.badlogic.gdx.utils.ObjectSet<String> set = new com.badlogic.gdx.utils.ObjectSet<>(30000, 0.75f); // 2.724 seconds taken.
+            String s = "\0";
+            for (int iter = 0; iter < 30; iter++) {
+                for (int i = 0; i < 1000; i++)
+                    set.add(s += '\0');
+            }
+            System.out.println(set.size());
+            System.out.println(((System.currentTimeMillis() - startTime) * 1E-3) + " seconds taken.");
+        }
+
     }
 }
